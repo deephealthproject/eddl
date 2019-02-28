0260f017f2eca9b1c5cee4cb805d6ddce6903806
Roberto Paredes <rparedes@dsic.upv.es>
2019-02-28 07:19:23 +0100

    reshapes

--- src/test/test_eddl_gpu.cpp
+++ src/test/test_eddl_gpu.cpp
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
   for(int i=0;i<3;i++)
       l=eddl.Activation(eddl.Dense(l,1024),"relu");
 
-  l=eddl.Reshape(l,{batch,32,-1});
+  l=eddl.Reshape(l,{batch,16,2,2,-1});
   l=eddl.Reshape(l,{batch,1024});
 
   layer out=eddl.Activation(eddl.Dense(l,10),"softmax");
