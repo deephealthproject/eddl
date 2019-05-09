ee315bda7a24ba52e824c5177e768ae82662e2a2
Salva Carrión <salva.carrion@outlook.com>
2019-05-09 12:50:49 +0200

    Minor clean up

--- src/eddl.h
+++ src/eddl.h
@@ -44,36 +44,36 @@
 #define initializer Initializer*
 #define compserv CompServ*
 
-typedef vector<LTensor*> vltensor;
+typedef vector<LTensor *> vltensor;
 
 class EDDL {
 public:
-  // Create Tensors
-  static tensor T(const initializer_list<int>& init);
-  static tensor T(const shape s);
-  static tensor T(string fname);
+    // Create Tensors
+    static tensor T(const initializer_list<int> &init);
+
+    static tensor T(const shape s);
+
+    static tensor T(string fname);
 
 
     //Operations tensors
-  static void div(tensor t,float v);
+    static void div(tensor t, float v);
 
 
     // Create Layers
-  static layer Input(tensor t);
-  static layer Input(const initializer_list<int>& init);
+    static layer Input(tensor t);
+    static layer Input(const initializer_list<int> &init);
 
     static layer Embedding(int input_dim, int output_dim, string name); //Todo: Implement
 
-  static layer Dense(layer parent,int dim);
-  static layer Dense(layer parent,int dim,string name);
-
+    static layer Dense(layer parent, int dim);
+    static layer Dense(layer parent, int dim, string name);
     static layer Dense(layer parent, int dim, string name, bool use_bias); //Todo: Implement
 
-  static layer Conv(layer parent,const initializer_list<int>& ks);
-  static layer Conv(layer parent,const initializer_list<int>& ks,const initializer_list<int>& st,string p);
-  static layer Conv(layer parent,const initializer_list<int>& ks,const initializer_list<int>& st);
-  static layer Conv(layer parent,const initializer_list<int>& ks,string p);
-
+    static layer Conv(layer parent, const initializer_list<int> &ks);
+    static layer Conv(layer parent, const initializer_list<int> &ks, const initializer_list<int> &st, string p);
+    static layer Conv(layer parent, const initializer_list<int> &ks, const initializer_list<int> &st);
+    static layer Conv(layer parent, const initializer_list<int> &ks, string p);
     static layer Conv(layer parent, int filters, const initializer_list<int> &kernel_size, string padding,
                       const initializer_list<int> &strides, int groups, const initializer_list<int> &dilation_rate,
                       bool use_bias, string name); //Todo: Implement
@@ -85,46 +85,45 @@ public:
     static layer UpSampling(layer parent, const initializer_list<int> &size, string interpolation,
                             string name); //Todo: Implement
 
+    static layer MPool(layer parent, const initializer_list<int> &ks);
+    static layer MPool(layer parent, const initializer_list<int> &ks, const initializer_list<int> &st, string p);
+    static layer MPool(layer parent, const initializer_list<int> &ks, const initializer_list<int> &st);
+    static layer MPool(layer parent, const initializer_list<int> &ks, string p);
 
-  static layer MPool(layer parent,const initializer_list<int>& ks);
-  static layer MPool(layer parent,const initializer_list<int>& ks,const initializer_list<int>& st,string p);
-  static layer MPool(layer parent,const initializer_list<int>& ks,const initializer_list<int>& st);
-  static layer MPool(layer parent,const initializer_list<int>& ks,string p);
+    static layer Activation(layer parent, string act);
+    static layer Activation(layer parent, string act, string name);
 
-  static layer Activation(layer parent,string act);
-  static layer Activation(layer parent,string act,string name);
-
-  static layer Reshape(layer parent, const initializer_list<int>& init);
-  static layer Reshape(layer parent, const initializer_list<int>& init,string name);
+    static layer Reshape(layer parent, const initializer_list<int> &init);
+    static layer Reshape(layer parent, const initializer_list<int> &init, string name);
 
     static layer Transpose(layer parent, const initializer_list<int> &dims, string name, int d); //Todo: Implement
 
-  static layer Drop(layer parent, float df);
-  static layer Drop(layer parent, float df,string name);
-
-    static layer Droprout(layer parent, float rate, string name, int d); //Todo: Implement
+    static layer Drop(layer parent, float df);
+    static layer Drop(layer parent, float df, string name);
+    static layer Dropout(layer parent, float rate, string name, int d); //Todo: Implement
 
     static layer BatchNormalization(layer parent, float momentum, float epsilon, bool affine,
                                     string name); //Todo: Implement
 
     static layer GaussianNoise(layer parent, float stddev, string name); //Todo: Implement
 
-
     //Todo: Implement -> static layer MaxPool(layer parent,const initializer_list<int>& pool_size,const initializer_list<int>& strides,string padding,string name);
     static layer GlobalMaxPool(layer parent, string name); //Todo: Implement
+
     static layer AveragePool(layer parent, const initializer_list<int> &pool_size, const initializer_list<int> &strides,
                              string padding, string name); //Todo: Implement
+
     static layer GlobalAveragePool(layer parent, string name); //Todo: Implement
 
 
     // ---- MERGE LAYERS ----
-    static layer Add(const initializer_list<layer>& init);
-  static layer Add(const initializer_list<layer>& init,string name);
+    static layer Add(const initializer_list<layer> &init);
+    static layer Add(const initializer_list<layer> &init, string name);
 
     static layer Substract(const initializer_list<layer> &layers, string name); //Todo: Implement
 
-  static layer Cat(const initializer_list<layer>& init);
-  static layer Cat(const initializer_list<layer>& init,string name);
+    static layer Cat(const initializer_list<layer> &init);
+    static layer Cat(const initializer_list<layer> &init, string name);
     //Todo: Implement -> static layer Concat(const initializer_list<layer>& layers,string name);
 
     static layer MatMul(const initializer_list<layer> &layers, string name); //Todo: Implement
@@ -134,8 +133,7 @@ public:
 
 
     // ---- OPTIMIZERS ----
-    static optimizer SGD(const initializer_list<float>& p);
-
+    static optimizer SGD(const initializer_list<float> &p);
     static optimizer SGD(float lr, float momentum, float weight_decay, bool nesterov); //Todo: Implement
     static optimizer Adam(float lr, float beta_1, float beta_2, float epsilon, float weight_decay,
                           bool amsgrad); //Todo: Implement
@@ -145,7 +143,7 @@ public:
     static optimizer Adamax(float lr, float beta_1, float beta_2, float epsilon, float weight_decay); //Todo: Implement
     static optimizer Nadam(float lr, float beta_1, float beta_2, float epsilon, float schedule_decay); //Todo: Implement
 
-  static void change(optimizer o,const initializer_list<float>& p);
+    static void change(optimizer o, const initializer_list<float> &p);
 
     // ---- LR SCHEDULERS ----
     static callback StepLR(int step_size, float gamma, int last_epoch); //Todo: Implement
@@ -165,26 +163,29 @@ public:
     static initializer GlorotUniform(float seed); //Todo: Implement
 
 
-  // Create net
-  static model Model(vlayer in,vlayer out);
-  // cs
-  ///////////
-  compserv CS_CPU(int th);
-  compserv CS_GPU(const initializer_list<int>& g);
-  compserv CS_FGPA(const initializer_list<int>& f);
+    // Create net
+    static model Model(vlayer in, vlayer out);
+    // cs
+    ///////////
+    compserv CS_CPU(int th);
+    compserv CS_GPU(const initializer_list<int> &g);
+    compserv CS_FGPA(const initializer_list<int> &f);
 
     // Layer operations
     static void set_trainable(layer l); //Todo: Implement
 
-  // Net operations
-  static void plot(model m,string fname);
-  static void info(model m);
-
+    // Net operations
+    static void plot(model m, string fname);
+    static void info(model m);
     static void summary(model m); //Todo: Implement
-  static void build(model net,optimizer o,const initializer_list<string>& c,const initializer_list<string>& m);
-  static void build(model net,optimizer o,const initializer_list<string>& c,const initializer_list<string>& m,CompServ *cs);
-  static void fit(model m, const initializer_list<LTensor*>& in,const initializer_list<LTensor*>& out,int batch,int epochs);
-  static void evaluate(model m, const initializer_list<LTensor*>& in,const initializer_list<LTensor*>& out);
+    static void build(model net, optimizer o, const initializer_list<string> &c, const initializer_list<string> &m);
+
+    static void build(model net, optimizer o, const initializer_list<string> &c, const initializer_list<string> &m, CompServ *cs);
+
+    static void
+    fit(model m, const initializer_list<LTensor *> &in, const initializer_list<LTensor *> &out, int batch, int epochs);
+
+    static void evaluate(model m, const initializer_list<LTensor *> &in, const initializer_list<LTensor *> &out);
 
     static void save_model(model m, string fname); //Todo: Implement
     static model load_model(string fname); //Todo: Implement
@@ -192,9 +193,8 @@ public:
     static void set_trainable(model m); //Todo: Implement
     static model zoo_models(string model_name); //Todo: Implement
 
-
-  // data
-  static void download_mnist();
+    // data
+    static void download_mnist();
 
     static void download_cifar10(); //Todo: Implement
     static void download_cifar100(); //Todo: Implement
