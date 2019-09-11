b25a37955da585e46ac1d74ea6c6216c29e1fa41
Roberto Paredes <rparedes@dsic.upv.es>
2019-09-11 16:52:28 +0200

    conv2D  cpu and gpu with bias

--- src/optimizers/optim_sgd.cpp
+++ src/optimizers/optim_sgd.cpp
@@ -61,10 +61,14 @@ void SGD::applygrads(int batch) {
     int p = 0;
 
     for (int i = 0; i < layers.size(); i++) {
+        //printf("=====\n%s\n=====\n",layers[i]->name.c_str());
+
         for (int j = 0; j < layers[i]->gradients.size(); j++, p++) {
+            //layers[i]->params[j]->info();
             Tensor::sum(lr / batch, layers[i]->gradients[j], mu, mT[p], mT[p], 0);
             Tensor::sum(1.0, layers[i]->params[j], 1.0, mT[p], layers[i]->params[j], 0);
         }
+        //printf("\n\n");
     }
     //getchar();
 
