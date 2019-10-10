07f75e3051b03d1de052f3da530f52fbeb440b2e
RParedesPalacios <rparedes@dsic.upv.es>
2019-10-10 15:21:48 +0200

    doomsday approaching

--- src/system_info.h
+++ src/system_info.h
@@ -3,9 +3,10 @@
 * Version: 0.1
 * copyright (c) 2019, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
 * Date: October 2019
-* Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
+* Author: Università degli studi di Modena e Reggio Emilia (federico.bolelli@unimore.it)
 * All rights reserved
 */
+
 #ifndef EDDL_SYTEM_INFO_H_
 #define EDDL_SYTEM_INFO_H_
 
