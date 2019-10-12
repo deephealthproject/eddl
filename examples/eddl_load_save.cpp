9141d80296eae919c6b0a881d4da5512b0266551
Roberto Paredes Palacios <rparedes@dsic.upv.es>
2019-10-12 18:39:36 +0200

    examples

---
+++ examples/eddl_load_save.cpp
@@ -0,0 +1,79 @@
+/*
+* EDDL Library - European Distributed Deep Learning Library.
+* Version: 0.1
+* copyright (c) 2019, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
+* Date: October 2019
+* Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
+* All rights reserved
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "apis/eddl.h"
+
+using namespace eddl;
+
+
+int main(int argc, char **argv) {
+
+    // Download dataset
+    download_mnist();
+
+    // Settings
+    int epochs = 1;
+    int batch_size = 100;
+    int num_classes = 10;
+
+    // Define network
+    layer in = Input({784});
+    layer l = in;  // Aux var
+
+    l = Activation(Dense(l, 1024), "relu");
+    l = Activation(Dense(l, 1024), "relu");
+    l = Activation(Dense(l, 1024), "relu");
+    layer out = Activation(Dense(l, num_classes), "softmax");
+    model net = Model({in}, {out});
+
+    // View model
+    summary(net);
+    plot(net, "model.pdf");
+
+    // Build model
+    build(net,
+          sgd(0.01, 0.9), // Optimizer
+          {"soft_cross_entropy"}, // Losses
+          {"categorical_accuracy"}, // Metrics
+          CS_CPU(4) // CPU with 4 threads
+    );
+
+    // Load dataset
+    tensor x_train = T_load("trX.bin");
+    tensor y_train = T_load("trY.bin");
+    tensor x_test = T_load("tsX.bin");
+    tensor y_test = T_load("tsY.bin");
+
+    // Preprocessing
+    div(x_train, 255.0);
+    div(x_test, 255.0);
+
+
+    save(net,"model1.bin");
+
+    // Train model
+    fit(net, {x_train}, {y_train}, batch_size, epochs);
+
+
+    load(net,"model1.bin");
+    fit(net, {x_train}, {y_train}, batch_size, epochs);
+
+    load(net,"model1.bin");
+    fit(net, {x_train}, {y_train}, batch_size, epochs);
+
+
+
+}
+
+
+///////////
