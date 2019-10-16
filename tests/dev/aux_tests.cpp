e4d8f2110acce21342a7960939bc7af82200e907
salvacarrion <salva.carrion@outlook.com>
2019-10-16 10:31:11 +0200

    Add Conv2D test

--- tests/dev/aux_tests.cpp
+++ tests/dev/aux_tests.cpp
@@ -33,7 +33,7 @@ bool check_tensors(Tensor* t_res, Tensor* t_sol){
     return Tensor::equal(t_res, t_sol);
 }
 
-TestResult run_mpool1(Tensor* t_input, int dev, int runs){
+TestResult run_mpool(Tensor* t_input, int dev, int runs){
     // Clone input tensor
     t_input = t_input->clone();
 
@@ -58,5 +58,35 @@ TestResult run_mpool1(Tensor* t_input, int dev, int runs){
     TestResult result;
     result.time = elapsed_secs;
     result.tensor = pd->O;
+    return result;
+}
+
+
+
+TestResult run_conv2d(Tensor* t_input, Tensor* t_kernel, int dev, int runs){
+    // Clone input tensor
+    t_input = t_input->clone();
+    t_kernel = t_kernel->clone();
+
+    // Move to device
+    if (dev == DEV_GPU){
+        t_input->ToGPU();
+        t_kernel->ToGPU();
+    }
+
+    // Instantiate PoolDescription + Perform MaxPooling
+    auto *cd = new ConvolDescriptor(1, {3, 3}, {1, 1}, "none");
+    cd->build(t_input);
+    cd->K = t_kernel;
+    clock_t begin = clock();
+    for(int i=0; i<runs; i++){
+        Conv2D(cd);
+    }
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+
+    TestResult result{};
+    result.time = elapsed_secs;
+    result.tensor = cd->O;
     return result;
 }
