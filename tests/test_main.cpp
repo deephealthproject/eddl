cc1517f012fb41c988fb5570f51148bead21bbcf
salvacarrion <salva.carrion@outlook.com>
2019-10-17 11:39:33 +0200

    Minor changes

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -49,119 +49,120 @@ int main(int argc, char **argv) {
     TestResult res_small_cpu, res_small_gpu, res_big_cpu, res_big_gpu;
     Tensor *t_input, *t_input_sol, *t_weights, *t_input_big, *t_weights_big;
     string act;
-//
-//    // *** [MAXPOOL] *****************************************
-//    // Data =================================
-//    float mpool_input[16] = {12.0, 20.0, 30.0, 0.0,
-//                             8.0, 12.0, 2.0, 0.0,
-//                             34.0, 70.0, 37.0, 4.0,
-//                             112.0, 100.0, 25.0, 12.0};
-//    float mpool_sol[4] = {20.0, 30.0,
-//                          112.0, 37.0};
-//    t_input = new Tensor({1, 1, 4, 4}, mpool_input, DEV_CPU);
-//    t_input_sol = new Tensor({1, 1, 2, 2}, mpool_sol, DEV_CPU);
-//    t_input_big = Tensor::randn({1, 1, 1000, 1000}, DEV_CPU);
-//
-//    res_small_cpu = run_mpool(t_input, DEV_CPU, 1);
-//    res_small_gpu = run_mpool(t_input, DEV_GPU, 1);
-//    res_big_cpu = run_mpool(t_input_big, DEV_CPU, 1);
-//    res_big_gpu = run_mpool(t_input_big, DEV_GPU, 1);
-//
-//    // Check correctness with solution
-//    print_results_ref("MaxPool2D", res_small_cpu, res_small_gpu, t_input_sol);
-//    print_results("MaxPool2D", res_big_cpu, res_big_gpu);
-//
-//
-//    // *** [CONV2D] *****************************************
-//    // Data =================================
-//    float conv2d_input[25] = {3.0, 3.0, 2.0, 1.0, 0.0,
-//                              0.0, 0.0, 1.0, 3.0, 1.0,
-//                              3.0, 1.0, 2.0, 2.0, 3.0,
-//                              2.0, 0.0, 0.0, 2.0, 2.0,
-//                              2.0, 0.0, 0.0, 0.0, 1.0};
-//    float conv2d_kernel[9] = {0.0, 1.0, 2.0,
-//                              2.0, 2.0, 0.0,
-//                              0.0, 1.0, 2.0};
-//    float conv2d_sol[9] = {12.0, 12.0, 17.0,
-//                           10.0, 17.0, 19.0,
-//                           9.0, 6.0, 14.0};
-//    t_input = new Tensor({1, 1, 5, 5}, conv2d_input, DEV_CPU);
-//    Tensor *t_conv2d_kernel = new Tensor({1, 1, 3, 3}, conv2d_kernel, DEV_CPU);
-//    t_input_sol = new Tensor({1, 1, 3, 3}, conv2d_sol, DEV_CPU);
-//    t_input_big =  Tensor::randn({1, 1, 10, 10}, DEV_CPU);
-//
-//    res_small_cpu = run_conv2d(t_input, t_conv2d_kernel, DEV_CPU, 1);
-//    res_small_gpu = run_conv2d(t_input, t_conv2d_kernel, DEV_GPU, 1);
-//    res_big_cpu = run_conv2d(t_input_big, t_conv2d_kernel, DEV_CPU, 1);
-//    res_big_gpu = run_conv2d(t_input_big, t_conv2d_kernel, DEV_GPU, 1);
-//
-//    // Check correctness with solution
-//    print_results_ref("Conv2D", res_small_cpu, res_small_gpu, t_input_sol);
-//    print_results("Conv2D", res_big_cpu, res_big_gpu);
-//
-//
-//    // *** [Dense] *****************************************
-//    // Data =================================
-//    int ndim = 3;
-//    int ndim_big = 1000*1000;
-//    t_input = Tensor::ones({1, 4}, DEV_CPU);
-//    t_weights = Tensor::full({4, ndim}, 2.0, DEV_CPU);
-//    t_input_sol = Tensor::full({1, 3}, 8.0, DEV_CPU);
-//    t_input_big = Tensor::ones({1, 4}, DEV_CPU);
-//    t_weights_big = Tensor::full({4, ndim_big}, 2.0, DEV_CPU);
-//
-//    res_small_cpu = run_dense(t_input, t_weights, DEV_CPU, 1);
-//    res_small_gpu = run_dense(t_input, t_weights, DEV_GPU, 1);
-//    res_big_cpu = run_dense(t_input_big, t_weights_big, DEV_CPU, 1);
-//    res_big_gpu = run_dense(t_input_big, t_weights_big, DEV_GPU, 1);
-//
-//    // Print results
-//    print_results_ref("Dense", res_small_cpu, res_small_gpu, t_input_sol);
-//    print_results("Dense", res_big_cpu, res_big_gpu);
-//
-//
-//    // *** [Sigmoid] *****************************************
-//    int vsize = 1000*1000;
-//    // Data =================================
-//    t_input_big = Tensor::randn({1, vsize}, DEV_CPU);
-//    act = "sigmoid";
-//
-//    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
-//    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
-//
-//    // Print results
-//    print_results(act, res_big_cpu, res_big_gpu);
-//
-//    // *** [Relu] *****************************************
-//    // Data =================================
-//    t_input_big = Tensor::randn({1, vsize}, DEV_CPU);
-//    act = "relu";
-//
-//    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
-//    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
-//
-//    // Print results
-//    print_results(act, res_big_cpu, res_big_gpu);
-//
-//
-//    // *** [Softmax] *****************************************
-//    // Data =================================
-//    t_input_big = Tensor::randn({1, vsize}, DEV_CPU);
-//    act = "softmax";
-//
-//    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
-//    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
-//
-//    // Print results
-//    print_results(act, res_big_cpu, res_big_gpu);
+
+    // *** [MAXPOOL] *****************************************
+    // Data =================================
+    float mpool_input[16] = {12.0, 20.0, 30.0, 0.0,
+                             8.0, 12.0, 2.0, 0.0,
+                             34.0, 70.0, 37.0, 4.0,
+                             112.0, 100.0, 25.0, 12.0};
+    float mpool_sol[4] = {20.0, 30.0,
+                          112.0, 37.0};
+    t_input = new Tensor({1, 1, 4, 4}, mpool_input, DEV_CPU);
+    t_input_sol = new Tensor({1, 1, 2, 2}, mpool_sol, DEV_CPU);
+    t_input_big = Tensor::randn({1, 1, 1000, 1000}, DEV_CPU);
+
+    res_small_cpu = run_mpool(t_input, DEV_CPU, 1);
+    res_small_gpu = run_mpool(t_input, DEV_GPU, 1);
+    res_big_cpu = run_mpool(t_input_big, DEV_CPU, 1);
+    res_big_gpu = run_mpool(t_input_big, DEV_GPU, 1);
+
+    // Check correctness with solution
+    print_results_ref("MaxPool2D", res_small_cpu, res_small_gpu, t_input_sol);
+    print_results("MaxPool2D", res_big_cpu, res_big_gpu);
+
+
+    // *** [CONV2D] *****************************************
+    // Data =================================
+    float conv2d_input[25] = {3.0, 3.0, 2.0, 1.0, 0.0,
+                              0.0, 0.0, 1.0, 3.0, 1.0,
+                              3.0, 1.0, 2.0, 2.0, 3.0,
+                              2.0, 0.0, 0.0, 2.0, 2.0,
+                              2.0, 0.0, 0.0, 0.0, 1.0};
+    float conv2d_kernel[9] = {0.0, 1.0, 2.0,
+                              2.0, 2.0, 0.0,
+                              0.0, 1.0, 2.0};
+    float conv2d_sol[9] = {12.0, 12.0, 17.0,
+                           10.0, 17.0, 19.0,
+                           9.0, 6.0, 14.0};
+    t_input = new Tensor({1, 1, 5, 5}, conv2d_input, DEV_CPU);
+    Tensor *t_conv2d_kernel = new Tensor({1, 1, 3, 3}, conv2d_kernel, DEV_CPU);
+    t_input_sol = new Tensor({1, 1, 3, 3}, conv2d_sol, DEV_CPU);
+    t_input_big =  Tensor::randn({1, 1, 10, 10}, DEV_CPU);
+    Tensor *t_conv2d_kernel_big = Tensor::randn({128, 512, 3, 3}, DEV_CPU);
+
+
+    res_small_cpu = run_conv2d(t_input, t_conv2d_kernel, DEV_CPU, 1);
+    res_small_gpu = run_conv2d(t_input, t_conv2d_kernel, DEV_GPU, 1);
+    res_big_cpu = run_conv2d(t_input_big, t_conv2d_kernel_big, DEV_CPU, 1);
+    res_big_gpu = run_conv2d(t_input_big, t_conv2d_kernel_big, DEV_GPU, 1);
+
+    // Check correctness with solution
+    print_results_ref("Conv2D", res_small_cpu, res_small_gpu, t_input_sol);
+    print_results("Conv2D", res_big_cpu, res_big_gpu);
+
+
+    // *** [Dense] *****************************************
+    // Data =================================
+    int ndim = 3;
+    int ndim_big = 1000*1000;
+    t_input = Tensor::ones({1, 4}, DEV_CPU);
+    t_weights = Tensor::full({4, ndim}, 2.0, DEV_CPU);
+    t_input_sol = Tensor::full({1, 3}, 8.0, DEV_CPU);
+    t_input_big = Tensor::ones({1, 4}, DEV_CPU);
+    t_weights_big = Tensor::full({4, ndim_big}, 2.0, DEV_CPU);
+
+    res_small_cpu = run_dense(t_input, t_weights, DEV_CPU, 1);
+    res_small_gpu = run_dense(t_input, t_weights, DEV_GPU, 1);
+    res_big_cpu = run_dense(t_input_big, t_weights_big, DEV_CPU, 1);
+    res_big_gpu = run_dense(t_input_big, t_weights_big, DEV_GPU, 1);
+
+    // Print results
+    print_results_ref("Dense", res_small_cpu, res_small_gpu, t_input_sol);
+    print_results("Dense", res_big_cpu, res_big_gpu);
+
+
+    // *** [Sigmoid] *****************************************
+    // Data =================================
+    t_input_big = Tensor::randn({100, 10000}, DEV_CPU);
+    act = "sigmoid";
+
+    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
+    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
+
+    // Print results
+    print_results(act, res_big_cpu, res_big_gpu);
+
+    // *** [Relu] *****************************************
+    // Data =================================
+    t_input_big = Tensor::randn({100, 10000}, DEV_CPU);
+    act = "relu";
+
+    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
+    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
+
+    // Print results
+    print_results(act, res_big_cpu, res_big_gpu);
+
+
+    // *** [Softmax] *****************************************
+    // Data =================================
+    t_input_big = Tensor::randn({10000, 100}, DEV_CPU);
+    act = "softmax";
+
+    res_big_cpu = run_activation(t_input_big, act, DEV_CPU, 1);
+    res_big_gpu = run_activation(t_input_big, act, DEV_GPU, 1);
+
+    // Print results
+    print_results(act, res_big_cpu, res_big_gpu);
 
     // *** [BathNorm] *****************************************
     // Data =================================
-    t_input_big = Tensor::ones({1, 1, 28, 28}, DEV_CPU);
+    t_input_big = Tensor::ones({1, 3, 1000, 1000}, DEV_CPU);
 
     res_big_cpu = run_batchnorm(t_input_big, DEV_CPU, 1);
-//    res_big_gpu = run_activation(t_input_big, DEV_GPU, 1);
+    res_big_gpu = run_batchnorm(t_input_big, DEV_GPU, 1);
 
     // Print results
-    print_results("BatchNorm", res_big_cpu, res_big_cpu);
+    print_results("BatchNorm", res_big_cpu, res_big_gpu);
 }
