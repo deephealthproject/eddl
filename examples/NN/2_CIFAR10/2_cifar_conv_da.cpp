435372f1bd2b962efa507f85095b7e0cbbe69dae
Roberto Paredes <rparedes@dsic.upv.es>
2019-11-11 14:46:03 +0100

    examples

---
+++ examples/NN/2_CIFAR10/2_cifar_conv_da.cpp
@@ -0,0 +1,114 @@
+/*
+* EDDL Library - European Distributed Deep Learning Library.
+* Version: 0.1
+* copyright (c) 2019, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
+* Date: October 2019
+* Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
+* All rights reserved
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "apis/eddl.h"
+#include "apis/eddlT.h"
+
+using namespace eddl;
+
+//////////////////////////////////
+// cifar_conv.cpp:
+// A very basic Conv for cifar10
+// Using fit for training
+//////////////////////////////////
+
+
+int main(int argc, char **argv){
+
+  // download CIFAR data
+  download_cifar10();
+
+  // Settings
+  int epochs = 25;
+  int batch_size = 100;
+  int num_classes = 10;
+
+  // network
+  layer in=Input({3,32,32});
+  layer l=in;
+
+  //    // Transformations
+  //    l = Shift(l, {2, 2});  // In pixels
+  //    l = Scale(l, {30, 30}, true); // In pixels
+  //    // Not implemented => l = Rotate(l, 15, false); // Degrees
+  //    l = Flip(l, 1);
+  //    l = Crop(l, {0, 0}, {23, 23}, false);  // In pixels
+  //    l = CropAndScale(l, {0, 0}, false);  // In pixels
+  //    l = Cutout(l, {0, 0}, {5, 5}); // In pixels
+
+
+  // Data augmentation
+  l = FlipRandom(l, 1);
+  l = ShiftRandom(l, {-0.1f, +0.1f}, {-0.1f, +0.1f});
+  l = ScaleRandom(l, {0.9f, 1.1f});
+  l = CropRandom(l, {28, 28});  //In pixels  // TODO: Needs a fix!
+
+  ///----
+  //l = CropAndScaleRandom(l, {0.8f, 1.0f});  // Crop and rescale  // TODO: check!
+  //l = CutoutRandom(l, {0.0f, 0.05f}, {0.0, 0.05f});
+
+
+
+  l=MaxPool(ReLu(Conv(l,32,{3,3},{1,1})),{2,2});
+  l=MaxPool(ReLu(Conv(l,64,{3,3},{1,1})),{2,2});
+  l=MaxPool(ReLu(Conv(l,128,{3,3},{1,1})),{2,2});
+  l=MaxPool(ReLu(Conv(l,256,{3,3},{1,1})),{2,2});
+
+  l=Reshape(l,{-1});
+
+  l=Activation(Dense(l,128),"relu");
+
+  layer out=Activation(Dense(l,num_classes),"softmax");
+
+  // net define input and output layers list
+  model net=Model({in},{out});
+
+
+  // Build model
+  build(net,
+    sgd(0.01, 0.9), // Optimizer
+    {"soft_cross_entropy"}, // Losses
+    {"categorical_accuracy"}, // Metrics
+    //CS_CPU() // CPU with maximum threads availables
+    CS_GPU({1}) // GPU with only one gpu
+  );
+
+  // plot the model
+  plot(net,"model.pdf");
+
+  // get some info from the network
+  summary(net);
+
+  // Load and preprocess training data
+  tensor x_train = eddlT::load("cifar_trX.bin");
+  tensor y_train = eddlT::load("cifar_trY.bin");
+  eddlT::div_(x_train, 255.0);
+
+  // Load and preprocess test data
+  tensor x_test = eddlT::load("cifar_tsX.bin");
+  tensor y_test = eddlT::load("cifar_tsY.bin");
+  eddlT::div_(x_test, 255.0);
+
+  for(int i=0;i<epochs;i++) {
+    // training, list of input and output tensors, batch, epochs
+    fit(net,{x_train},{y_train},batch_size, 1);
+    // Evaluate train
+    std::cout << "Evaluate test:" << std::endl;
+    evaluate(net,{x_test},{y_test});
+  }
+
+
+}
+
+
+///////////
