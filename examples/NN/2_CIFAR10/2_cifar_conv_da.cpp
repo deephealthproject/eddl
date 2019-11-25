08fddc1a7f0b6d5296b47c71f15b6a3be17d3c6e
salvacarrion <salva.carrion@outlook.com>
2019-11-25 13:27:46 +0100

    Fix DA methods (CropScale not working)

--- examples/NN/2_CIFAR10/2_cifar_conv_da.cpp
+++ examples/NN/2_CIFAR10/2_cifar_conv_da.cpp
@@ -38,12 +38,12 @@ int main(int argc, char **argv){
     layer l=in;
 
     // Data augmentation
-    l = ShiftRandom(l, {-0.2f, +0.2f}, {-0.2f, +0.2f});
+//    l = ShiftRandom(l, {-0.2f, +0.2f}, {-0.2f, +0.2f});
 //    l = RotateRandom(l, {-30.0f, +30.0f});
-//    l = ScaleRandom(l, {0.8f, 1.5f});
+//    l = ScaleRandom(l, {0.85f, 2.0f});
 //    l = FlipRandom(l, 1);
 //    l = CropRandom(l, {28, 28});
-//    l = CropAndScaleRandom(l, {0.8f, 1.0f});
+    l = CropAndScaleRandom(l, {0.8f, 1.0f});
 //    l = CutoutRandom(l, {0.0f, 0.3f}, {0.0f, 0.3f});
 
     l=MaxPool(ReLu(Conv(l,32,{3,3},{1,1})),{2,2});
