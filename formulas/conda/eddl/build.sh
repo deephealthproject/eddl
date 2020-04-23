da989038bc51d26e3a888a7d7e03585871d6fd94
Salva Carrión <salva.carrion@outlook.com>
2020-04-23 02:55:06 +0200

    Compiler fixes for conda

--- formulas/conda/eddl/build.sh
+++ formulas/conda/eddl/build.sh
@@ -8,6 +8,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     MACOSX_DEPLOYMENT_TARGET=10.9
     CXX=clang++
     CC=clang
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    CXX=g++
+    CC=gcc
 fi
 
 
@@ -16,5 +19,8 @@ mkdir build
 cd build/
 cmake -DBUILD_TARGET=CPU -DBUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 
-# Compile and install
-make install -j8
+# Compile
+make -j${CPU_COUNT} ${VERBOSE_CM}
+
+# Install
+make install -j${CPU_COUNT}
