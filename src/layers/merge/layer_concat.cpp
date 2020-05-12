0ba0f6130083fa43ede9abab81a0f40890bb8a5f
salvacarrion <salva.carrion@outlook.com>
2020-05-12 19:58:23 +0200

    Improve Concat

--- src/layers/merge/layer_concat.cpp
+++ src/layers/merge/layer_concat.cpp
@@ -24,21 +24,21 @@ LConcat::LConcat(vector<Layer *> parent, unsigned int axis, string name, int dev
 
     // Perform layer checks
     if (parent.empty()) { msg("Error: LConcat layer with empty list"); }
-    this->ndim = parent[0]->output->ndim;
+
     this->axis = axis;
 
     if (parent.size() > 1) {
 
         // All layers need to have the same number of dimensions
+        int ndim = parent[0]->output->ndim;
         for (int i = 0; i < parent.size() - 1; ++i) {
-            if (this->ndim != parent[i+1]->output->ndim){
+            if (ndim != parent[i + 1]->output->ndim){
                 msg("Error: LConcat layers with different tensor dims");
             }
         }
-
         // Check dimensions
         for (int i = 0; i < parent.size() - 1; ++i){
-            for (int d = 0; d < this->ndim; ++d) {
+            for (int d = 0; d < ndim; ++d) {
                 if (d != this->axis && parent[i]->output->shape[d] != parent[i + 1]->output->shape[d]) {
                     msg("Error: LConcat layers with different size in dim 1");
                 }
@@ -53,7 +53,6 @@ LConcat::LConcat(vector<Layer *> parent, unsigned int axis, string name, int dev
     int t = 0;
     for (int i = 0; i < parent.size(); ++i) {
         t += parent[i]->output->shape[axis];
-        index.push_back(t);
     }
 
     // Set new shape
@@ -62,32 +61,6 @@ LConcat::LConcat(vector<Layer *> parent, unsigned int axis, string name, int dev
 
     input = parent[0]->output;
     output = new Tensor(new_shape, dev);
-//    if (!mem_level) { delta = new Tensor(output->shape, dev);  }  // NOT parent[0]->output
-
-    // Temp indices
-    vector<string> indices_tmp;
-    for (int i = 0; i < parent.size(); ++i) {
-        indices_tmp.push_back(":");
-    }
-
-    // Create a descriptor for each layer address translation
-    int start_idx = 0;
-    int end_idx = 0;
-    for (int i = 0; i < parent.size(); ++i) {
-        // Build indices for the given axis
-        start_idx = end_idx;
-        end_idx = start_idx+parent[i]->output->shape[axis];
-        vector<string> indices = vector<string>(indices_tmp);
-        indices[axis] = to_string(start_idx)+":"+to_string(end_idx);
-
-        // Build descriptor
-        auto *aux = new SelDescriptor(indices);
-
-        // Build translation table
-        aux->build(this->output->shape);
-        aux->build_indices();
-        this->sd.push_back(aux);
-    }
 
     // Set children
     for (int i = 0; i < parent.size(); ++i) {
