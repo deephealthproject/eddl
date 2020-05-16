daacd49e6e5365b91a6ee27c36c21a6c3cfef110
Roberto Paredes Palacios <rparedes@dsic.upv.es>
2020-05-16 10:02:00 +0200

    examples

--- examples/nn/1_mnist/14_mnist_losses.cpp
+++ examples/nn/1_mnist/14_mnist_losses.cpp
@@ -84,9 +84,9 @@ int main(int argc, char **argv) {
           adam(0.001), // Optimizer
           {}, // Losses
           {}, // Metrics
-          CS_GPU({1}) // one GPU
+          //CS_GPU({1}) // one GPU
           //CS_GPU({1,1},100) // two GPU with weight sync every 100 batches
-          //CS_CPU()
+          CS_CPU()
     );
 
     summary(net);
