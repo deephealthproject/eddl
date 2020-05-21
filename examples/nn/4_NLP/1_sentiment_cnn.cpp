699a75b29d65ff72b82ae20013c8c493bccc5a37
Roberto Paredes Palacios <rparedes@dsic.upv.es>
2020-05-21 21:17:36 +0200

    cnn imdb

--- examples/nn/4_NLP/1_sentiment_cnn.cpp
+++ examples/nn/4_NLP/1_sentiment_cnn.cpp
@@ -23,68 +23,67 @@ using namespace eddl;
 //////////////////////////////////
 
 int main(int argc, char **argv) {
-    // Download Imdb
-    download_imdb_2000();
+  // Download Imdb
+  download_imdb_2000();
 
-    // Settings
-    int epochs = 10;
-    int batch_size = 100;
+  // Settings
+  int epochs = 10;
+  int batch_size = 32;
 
-    int length=250;
-    int embdim=32;
-    int vocsize=2000;
+  int length=250;
+  int embdim=32;
+  int vocsize=2000;
 
-    // Define network
-    layer in = Input({length});
-    layer l = in;
+  // Define network
+  layer in = Input({length});
+  layer l = in;
 
-    layer lE = RandomUniform(Embedding(l, vocsize, length,embdim),-0.05,0.05);
+  layer lE = RandomUniform(Embedding(l, vocsize, length,embdim),-0.05,0.05);
+  l = Reshape(lE,{1,length,embdim});
 
-    l = Reshape(lE,{1,length,embdim});
+  layer l1 = ReLu(BatchNormalization(Conv(l,128,{1,embdim},{1,1},"same,none",false)));
+  layer l2 = ReLu(BatchNormalization(Conv(l,128,{2,embdim},{1,1},"same,none",false)));
+  layer l3 = ReLu(BatchNormalization(Conv(l,128,{3,embdim},{1,1},"same,none",false)));
 
-    layer l1 = ReLu(BatchNormalization(Conv(l,128,{1,embdim},{1,1},"same,none",false)));
-    layer l2 = ReLu(BatchNormalization(Conv(l,128,{2,embdim},{1,1},"same,none",false)));
-    layer l3 = ReLu(BatchNormalization(Conv(l,128,{3,embdim},{1,1},"same,none",false)));
+  layer lc1=l=Concat({l1,l2,l3});
 
-    layer lc1=l=Concat({l1,l2,l3});
+  l=GlobalMaxPool(l);
 
-    l=GlobalMaxPool(l);
+  l = Reshape(l,{-1});
 
-    l = Reshape(l,{-1});
+  l = ReLu(BatchNormalization(Dense(l,256)));
 
-    l = ReLu(BatchNormalization(Dense(l,256)));
+  layer out = Sigmoid(Dense(l, 1));
+  model net = Model({in}, {out});
 
-    layer out = Sigmoid(Dense(l, 1));
-    model net = Model({in}, {out});
+  // dot from graphviz should be installed:
+  plot(net, "model.pdf");
 
-    // dot from graphviz should be installed:
-    plot(net, "model.pdf");
+  // Build model
+  build(net,
+    adam(0.0001), // Optimizer
+    {"cross_entropy"}, // Losses
+    {"binary_accuracy"}, // Metrics
+    //CS_GPU({1}) // one GPU
+    //CS_GPU({1,1},100) // two GPU with weight sync every 100 batches
+    CS_CPU()
+  );
+  // View model
+  summary(net);
 
-    // Build model
-    build(net,
-          adam(0.001), // Optimizer
-          {"soft_cross_entropy"}, // Losses
-          {"binary_accuracy"}, // Metrics
-          //CS_GPU({1}) // one GPU
-          //CS_GPU({1,1},100) // two GPU with weight sync every 100 batches
-          CS_CPU()
-    );
-    // View model
-    summary(net);
+  // Load dataset
+  Tensor* x_train=Tensor::load("imdb_2000_trX.bin");
+  Tensor* y_train=Tensor::load("imdb_2000_trY.bin");
 
-    // Load dataset
-    Tensor* x_train=Tensor::load("imdb_2000_trX.bin");
-    Tensor* y_train=Tensor::load("imdb_2000_trY.bin");
-
-    Tensor* x_test=Tensor::load("imdb_2000_tsX.bin");
-    Tensor* y_test=Tensor::load("imdb_2000_tsY.bin");
+  Tensor* x_test=Tensor::load("imdb_2000_tsX.bin");
+  Tensor* y_test=Tensor::load("imdb_2000_tsY.bin");
 
 
-    // Train model
-    for(int i=0;i<epochs;i++) {
-      fit(net, {x_train}, {y_train}, batch_size, 1);
-      evaluate(net,{x_test},{y_test});
-    }
+  // Train model
+  for(int i=0;i<epochs;i++) {
+    fit(net, {x_train}, {y_train}, batch_size, 1);
+    evaluate(net,{x_test},{y_test});
+  }
 
 
 
