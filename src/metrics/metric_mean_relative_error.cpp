b84ce41f98e2323d60c15fc644bbf87e646ef094
Salva Carrión <salva.carrion@outlook.com>
2020-06-03 15:11:17 +0200

    Update version

--- src/metrics/metric_mean_relative_error.cpp
+++ src/metrics/metric_mean_relative_error.cpp
@@ -1,6 +1,6 @@
 /*
 * EDDL Library - European Distributed Deep Learning Library.
-* Version: 0.6
+* Version: 0.7
 * copyright (c) 2020, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
 * Date: April 2020
 * Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
