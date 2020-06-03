4734fc5ce35cdf1977fb5fdfd436cd9e53637cc6
Jose Flich <jflich@disca.upv.es>
2020-06-03 15:40:44 +0200

    added new skeletons

---
+++ src/hardware/fpga/kernels/bn.cpp
@@ -0,0 +1,16 @@
+void k_permute_channels_last(Tensor *A,Tensor *B)
+{
+}
+
+void k_permute_channels_first(Tensor *A,Tensor *B)
+{
+}
+
+void k_permute_batch_last(Tensor *A,Tensor *B)
+{
+}
+
+void k_permute_batch_first(Tensor *A,Tensor *B)
+{
+}
+
---
+++ src/hardware/fpga/kernels/conv.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+extern "C" {
+
+void k_conv2D(ConvolDescriptor *D)
+{
+}
+
+void k_conv2D_grad(ConvolDescriptor *D)
+{
+}
+
+void k_conv2D_back(ConvolDescriptor *D)
+{
+}
+
+}
