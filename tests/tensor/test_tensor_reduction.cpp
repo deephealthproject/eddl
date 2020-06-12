b3211f60fc24df9ccc9649d0adcc1fa2329cf9cd
Salva Carrión <salva.carrion@outlook.com>
2020-06-12 17:04:25 +0200

    Add test for sum reduce

--- tests/tensor/test_tensor_reduction.cpp
+++ tests/tensor/test_tensor_reduction.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <random>
 #include <string>
+#include <ctime>
 
 #include "eddl/tensor/tensor.h"
 #include "eddl/tensor/tensor_reduction.h"
@@ -10,15 +11,17 @@
 
 using namespace std;
 
-//
-//TEST(TensorTestSuite, tensor_math_reduction_sum) {
-//    // Test #1
-//
-//    Tensor *t1 = Tensor::ones({3, 2, 4}, DEV_CPU);
-//    t1->print();
-//
-//    auto RD = new ReduceDescriptor(t1, {2}, "sum", false);
-//    reduction(RD);
-//    RD->O->print();
-//}
-//
+
+TEST(TensorTestSuite, tensor_math_reduction_sum) {
+    // Test #1
+    Tensor *t1_ref = new Tensor({4.0f, 4.0f, 4.0f,
+                                      4.0f, 4.0f, 4.0f}, {3, 2}, DEV_CPU);
+    Tensor *t1 = Tensor::ones({3, 2, 4}, DEV_CPU);
+    t1->print();
+
+    Tensor *new_t = t1->sum({2}, false);
+    new_t->print();
+    ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
+
+}
+
