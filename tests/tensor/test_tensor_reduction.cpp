83883545826d6ca4fa919b47a2ad7f9007f7e04c
Salva Carrión <salva.carrion@outlook.com>
2020-06-15 12:56:21 +0200

    Add prod reduction (test)

--- tests/tensor/test_tensor_reduction.cpp
+++ tests/tensor/test_tensor_reduction.cpp
@@ -18,7 +18,6 @@ TEST(TensorTestSuite, tensor_math_reduction_max) {
     Tensor *t1_ref = new Tensor({6.0f, 7.0f, 4.0f},  {3}, DEV_CPU);
     Tensor *t1 = new Tensor({4.0f, 7.0f, 3.0f,
                                   6.0f, 4.0f, 4.0f}, {2, 3}, DEV_CPU);
-    t1->print();
 
     Tensor *new_t = t1->max({0}, false);
     ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
@@ -30,7 +29,6 @@ TEST(TensorTestSuite, tensor_math_reduction_min) {
     Tensor *t1_ref = new Tensor({4.0f, 4.0f, 3.0f},  {3}, DEV_CPU);
     Tensor *t1 = new Tensor({4.0f, 7.0f, 3.0f,
                              6.0f, 4.0f, 4.0f}, {2, 3}, DEV_CPU);
-    t1->print();
 
     Tensor *new_t = t1->min({0}, false);
     ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
@@ -42,7 +40,6 @@ TEST(TensorTestSuite, tensor_math_reduction_sum) {
                                  4.0f, 4.0f,
                                  4.0f, 4.0f}, {3, 2}, DEV_CPU);
     Tensor *t1 = Tensor::ones({3, 2, 4}, DEV_CPU);
-    t1->print();
 
     Tensor *new_t = t1->sum({2}, false);
     ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
@@ -54,9 +51,17 @@ TEST(TensorTestSuite, tensor_math_reduction_sum_abs) {
     Tensor *t1_ref = new Tensor({10.0f, 11.0f, 7.0f},  {3}, DEV_CPU);
     Tensor *t1 = new Tensor({-4.0f, 7.0f, 3.0f,
                              6.0f, 4.0f, -4.0f}, {2, 3}, DEV_CPU);
-    t1->print();
 
     Tensor *new_t = t1->sum_abs({0}, false);
     ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
 }
 
+TEST(TensorTestSuite, tensor_math_reduction_prod) {
+    // Test #1
+    Tensor *t1_ref = new Tensor({24.0f, 28.0f, 12.0f},  {3}, DEV_CPU);
+    Tensor *t1 = new Tensor({4.0f, 7.0f, 3.0f,
+                             6.0f, 4.0f, 4.0f}, {2, 3}, DEV_CPU);
+
+    Tensor *new_t = t1->prod({0}, false);
+    ASSERT_TRUE(Tensor::equivalent(t1_ref, new_t, 10e-4));
+}
