1c3f299537f5edc591b48869d66b11311fb70f47
Jose Flich <jflich@disca.upv.es>
2020-06-16 10:33:56 +0000

    added fpga kernels

--- fpga_kernels/kernel_eye.cpp
+++ fpga_kernels/kernel_eye.cpp
@@ -1,27 +1,7 @@
 #include <math.h>
 #include <stdio.h>
-#include "../../../../include/eddl/hardware/fpga/fpga_enables.h"
 extern "C" {
 
-#ifdef K_ENABLED_RANGE
-void k_range(float *A, float min, float step, long int size){
-
-  #pragma HLS INTERFACE m_axi port=A offset=slave bundle=gmem
-  #pragma HLS INTERFACE s_axilite port=A  bundle=control
-  #pragma HLS INTERFACE s_axilite port=min bundle=control
-  #pragma HLS INTERFACE s_axilite port=step bundle=control
-  #pragma HLS INTERFACE s_axilite port=size bundle=control
-
-   float v=min;
-
-    for(int i=0; i<size; i++){
-        A[i] = v;
-        v+=step;
-    }
-}
-#endif
-
-#ifdef K_ENABLED_EYE
 void k_eye(float *A, int offset, long int size, int Ashape0, int Ashape1){
 
   #pragma HLS INTERFACE m_axi port=A offset=slave bundle=gmem
@@ -36,6 +16,5 @@ void k_eye(float *A, int offset, long int size, int Ashape0, int Ashape1){
         else { A[i] = 0.0f; }
     }
 }
-#endif
 
 }
