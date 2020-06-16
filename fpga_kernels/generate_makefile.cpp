988a4a957e42c8ada08a6ffa3fa5dcfe807762d1
Jose Flich <jflich@disca.upv.es>
2020-06-16 05:48:58 +0000

    finished generate_makefile.cpp

--- fpga_kernels/generate_makefile.cpp
+++ fpga_kernels/generate_makefile.cpp
@@ -9,45 +9,420 @@ int main(int argc, char **argv) {
   char szKernels[200][50];
   int num_kernels = 0;
 
-#ifdef K_ENABLED_RELU
+  // Activations
+  #ifdef K_ENABLED_RELU
   strcpy(szKernels[num_kernels++], "relu");
-#endif
-#ifdef K_ENABLED_D_RELU
+  #endif
+  #ifdef K_ENABLED_D_RELU
   strcpy(szKernels[num_kernels++], "d_relu");
-#endif
-#ifdef K_ENABLED_ELU
+  #endif
+  #ifdef K_ENABLED_THRESHOLDED_RELU
+  strcpy(szKernels[num_kernels++], "thresholded_relu");
+  #endif
+  #ifdef K_ENABLED_D_TRHESHOLDED_RELU
+  strcpy(szKernels[num_kernels++], "d_thresholded_relu");
+  #endif
+  #ifdef K_ENABLED_LEAKY_RELU
+  strcpy(szKernels[num_kernels++], "leaky_relu");
+  #endif
+  #ifdef K_ENABLED_D_LEAKY_RELU
+  strcpy(szKernels[num_kernels++], "d_leaky_relu");
+  #endif
+  #ifdef K_ENABLED_ELU
   strcpy(szKernels[num_kernels++], "elu");
-#endif
-#ifdef K_ENABLED_D_ELU
-  strcpy(szKernels[num_kernels++], "elu");
-#endif
-#ifdef K_ENABLED_MUL_
-  strcpy(szKernels[num_kernels++], "mul_");
-#endif
-#ifdef K_ENABLED_ADD
-  strcpy(szKernels[num_kernels++], "add");
-#endif
-#ifdef K_ENABLED_MUL2D
-  strcpy(szKernels[num_kernels++], "mul2d");
-#endif
-#ifdef K_ENABLED_SUM2D_ROWWISE
-  strcpy(szKernels[num_kernels++], "sum2d_rowwise");
-#endif  
-#ifdef K_ENABLED_SUM
-  strcpy(szKernels[num_kernels++], "sum");
-#endif
-#ifdef K_ENABLED_CENT
-  strcpy(szKernels[num_kernels++], "cent");
-#endif
-#ifdef K_ENABLED_ACCURACY
-  strcpy(szKernels[num_kernels++], "accuracy");
-#endif  
-#ifdef K_ENABLED_REDUCE_SUM2D
-  strcpy(szKernels[num_kernels++], "reduce_sum2d");
-#endif
-#ifdef K_ENABLED_SOFTMAX
+  #endif
+  #ifdef K_ENABLED_D_ELU
+  strcpy(szKernels[num_kernels++], "d_elu");
+  #endif
+  #ifdef K_ENABLED_SOFTPLUS
+  strcpy(szKernels[num_kernels++], "softplus");
+  #endif
+  #ifdef K_ENABLED_D_SOFTPLUS
+  strcpy(szKernels[num_kernels++], "d_softplus");
+  #endif
+  #ifdef K_ENABLED_SOFTSIGN
+  strcpy(szKernels[num_kernels++], "softsign");
+  #endif
+  #ifdef K_ENABLED_D_SOFTSIGN
+  strcpy(szKernels[num_kernels++], "d_softsign");
+  #endif
+  #ifdef K_ENABLED_LINEAR
+  strcpy(szKernels[num_kernels++], "linear");
+  #endif
+  #ifdef K_ENABLED_D_LINEAR
+  strcpy(szKernels[num_kernels++], "d_linear");
+  #endif
+  #ifdef K_ENABLED_SIGMOID
+  strcpy(szKernels[num_kernels++], "sigmoid");
+  #endif
+  #ifdef K_ENABLED_D_SIGMOID
+  strcpy(szKernels[num_kernels++], "d_sigmoid");
+  #endif
+  #ifdef K_ENABLED_HARD_SIGMOID
+  strcpy(szKernels[num_kernels++], "hard_sigmoid");
+  #endif
+  #ifdef K_ENABLED_D_HARD_SIGMOID
+  strcpy(szKernels[num_kernels++], "d_hard_sigmoid");
+  #endif
+  #ifdef K_ENABLED_EXP
+  strcpy(szKernels[num_kernels++], "exp");
+  #endif
+  #ifdef K_ENABLED_D_EXP
+  strcpy(szKernels[num_kernels++], "d_exp");
+  #endif
+  #ifdef K_ENABLED_TANH
+  strcpy(szKernels[num_kernels++], "tanh");
+  #endif
+  #ifdef K_ENABLED_D_TANH
+  strcpy(szKernels[num_kernels++], "d_tanh");
+  #endif
+  #ifdef K_ENABLED_SOFTMAX
   strcpy(szKernels[num_kernels++], "softmax");
-#endif
+  #endif
+  #ifdef K_ENABLED_D_SOFTMAX
+  strcpy(szKernels[num_kernels++], "d_softmax");
+  #endif
+
+  // Bn
+  #ifdef K_ENABLED_PERMUTE_CHANNELS_LAST
+  strcpy(szKernels[num_kernels++], "permute_channels_last");
+  #endif
+  #ifdef K_ENABLED_PERMUTE_CHANNELS_FIRST
+  strcpy(szKernels[num_kernels++], "d_permute_channels_last");
+  #endif
+  #ifdef K_ENABLED_PERMUTE_BATCH_LAST
+  strcpy(szKernels[num_kernels++], "permute_batch_last");
+  #endif
+  #ifdef K_ENABLED_PERMUTE_BATCH_FIRST
+  strcpy(szKernels[num_kernels++], "permute_batch_first");
+  #endif
+
+  // Comparison
+  #ifdef K_ENABLED_ALL
+  strcpy(szKernels[num_kernels++], "all");
+  #endif
+  #ifdef K_ENABLED_ANY
+  strcpy(szKernels[num_kernels++], "any");
+  #endif
+  #ifdef K_ENABLED_ISFINITE
+  strcpy(szKernels[num_kernels++], "isfinite");
+  #endif
+  #ifdef K_ENABLED_ISINF
+  strcpy(szKernels[num_kernels++], "isinf");
+  #endif
+  #ifdef K_ENABLED_ISNAN
+  strcpy(szKernels[num_kernels++], "isnan");
+  #endif
+  #ifdef K_ENABLED_ISNEGINF
+  strcpy(szKernels[num_kernels++], "isneginf");
+  #endif
+  #ifdef K_ENABLED_ISPOSINF
+  strcpy(szKernels[num_kernels++], "isposinf");
+  #endif
+  #ifdef K_ENABLED_LOGICAL_AND
+  strcpy(szKernels[num_kernels++], "logical_and");
+  #endif
+  #ifdef K_ENABLED_LOGICAL_OR
+  strcpy(szKernels[num_kernels++], "logical_or");
+  #endif
+  #ifdef K_ENABLED_LOGICAL_NOT
+  strcpy(szKernels[num_kernels++], "logical_not");
+  #endif
+  #ifdef K_ENABLED_LOGICAL_XOR
+  strcpy(szKernels[num_kernels++], "logigal_xor");
+  #endif
+  #ifdef K_ENABLED_ALLCLOSE
+  strcpy(szKernels[num_kernels++], "allclose");
+  #endif
+  #ifdef K_ENABLED_ISCLOSE
+  strcpy(szKernels[num_kernels++], "isclose");
+  #endif
+  #ifdef K_ENABLED_GREATER
+  strcpy(szKernels[num_kernels++], "greater");
+  #endif
+  #ifdef K_ENABLED_GREATER_EQUAL
+  strcpy(szKernels[num_kernels++], "greater_equal");
+  #endif
+  #ifdef K_ENABLED_LESS
+  strcpy(szKernels[num_kernels++], "less");
+  #endif
+  #ifdef K_ENABLED_LESS_EQUAL
+  strcpy(szKernels[num_kernels++], "less_equal");
+  #endif
+  #ifdef K_ENABLED_EQUAL
+  strcpy(szKernels[num_kernels++], "equal");
+  #endif
+  #ifdef K_ENABLED_NOT_EQUAL
+  strcpy(szKernels[num_kernels++], "not_equal");
+  #endif
+  #ifdef K_ENABLED_EQUAL2
+  strcpy(szKernels[num_kernels++], "equal2");
+  #endif
+
+  // Conv
+  #ifdef K_ENABLED_IM2COL
+  strcpy(szKernels[num_kernels++], "im2col");
+  #endif
+
+  // Core
+  #ifdef K_ENABLED_FILL_
+  strcpy(szKernels[num_kernels++], "fill_");
+  #endif
+  #ifdef K_ENABLED_FILL
+  strcpy(szKernels[num_kernels++], "fill");
+  #endif
+  #ifdef K_ENABLED_SELECT
+  strcpy(szKernels[num_kernels++], "select");
+  #endif
+  #ifdef K_ENABLED_SELECT_BACK
+  strcpy(szKernels[num_kernels++], "select_back");
+  #endif
+  #ifdef K_ENABLED_SET_SELECT
+  strcpy(szKernels[num_kernels++], "set_select");
+  #endif
+  #ifdef K_ENABLED_SET_SELECT_BACK
+  strcpy(szKernels[num_kernels++], "set_select_back");
+  #endif
+  #ifdef K_ENABLED_SELECT2
+  strcpy(szKernels[num_kernels++], "select2");
+  #endif
+  #ifdef K_ENABLED_DESELECT
+  strcpy(szKernels[num_kernels++], "deselect");
+  #endif
+  #ifdef K_ENABLED_CONCAT
+  strcpy(szKernels[num_kernels++], "concat");
+  #endif
+
+  // Create
+  #ifdef K_ENABLED_RANGE
+  strcpy(szKernels[num_kernels++], "range");
+  #endif
+  #ifdef K_ENABLED_EYE
+  strcpy(szKernels[num_kernels++], "eye");
+  #endif
+
+  // Da
+  #ifdef K_ENABLED_SINGLE_SHIFT
+  strcpy(szKernels[num_kernels++], "single_shift");
+  #endif
+  #ifdef K_ENABLED_SINGLE_ROTATE
+  strcpy(szKernels[num_kernels++], "single_rotate");
+  #endif
+  #ifdef K_ENABLED_SINGLE_SCALE
+  strcpy(szKernels[num_kernels++], "single_scale");
+  #endif
+  #ifdef K_ENABLED_SINGLE_FLIP
+  strcpy(szKernels[num_kernels++], "single_flip");
+  #endif
+  #ifdef K_ENABLED_SINGLE_CROP
+  strcpy(szKernels[num_kernels++], "single_crop");
+  #endif
+  #ifdef K_ENABLED_SINGLE_CROP_SCALE
+  strcpy(szKernels[num_kernels++], "single_crop_scale");
+  #endif
+
+  // Generator
+  #ifdef K_ENABLED_RAND_UNIFORM
+  strcpy(szKernels[num_kernels++], "rand_uniform");
+  #endif
+  #ifdef K_ENABLED_RAND_SIGNED_UNIFORM
+  strcpy(szKernels[num_kernels++], "rand_signed_uniform");
+  #endif
+  #ifdef K_ENABLED_RAND_BINARY
+  strcpy(szKernels[num_kernels++], "rand_binary");
+  #endif
+  #ifdef K_ENABLED_RAND_NORMAL
+  strcpy(szKernels[num_kernels++], "rand_normal");
+  #endif
+
+  // Losses
+  #ifdef K_ENABLED_CENT
+  strcpy(szKernels[num_kernels++], "cent");
+  #endif
+
+  // Math
+  #ifdef K_ENABLED_ABS_
+  strcpy(szKernels[num_kernels++], "abs_");
+  #endif
+  #ifdef K_ENABLED_ACOS_
+  strcpy(szKernels[num_kernels++], "acos_");
+  #endif
+  #ifdef K_ENABLED_ADD_
+  strcpy(szKernels[num_kernels++], "add_");
+  #endif
+  #ifdef K_ENABLED_ASIN_
+  strcpy(szKernels[num_kernels++], "asin_");
+  #endif
+  #ifdef K_ENABLED_ATAN_
+  strcpy(szKernels[num_kernels++], "atan_");
+  #endif
+  #ifdef K_ENABLED_CEIL_
+  strcpy(szKernels[num_kernels++], "ceil_");
+  #endif
+  #ifdef K_ENABLED_CLAMP_
+  strcpy(szKernels[num_kernels++], "clamp_");
+  #endif
+  #ifdef K_ENABLED_COS_
+  strcpy(szKernels[num_kernels++], "cos_");
+  #endif
+  #ifdef K_ENABLED_COSH_
+  strcpy(szKernels[num_kernels++], "cosh_");
+  #endif
+  #ifdef K_ENABLED_EXP_
+  strcpy(szKernels[num_kernels++], "exp_");
+  #endif
+  #ifdef K_ENABLED_FLOOR_
+  strcpy(szKernels[num_kernels++], "floor_");
+  #endif
+  #ifdef K_ENABLED_INV_
+  strcpy(szKernels[num_kernels++], "inv_");
+  #endif
+  #ifdef K_ENABLED_LOG_
+  strcpy(szKernels[num_kernels++], "log_");
+  #endif
+  #ifdef K_ENABLED_LOG2_
+  strcpy(szKernels[num_kernels++], "log2_");
+  #endif
+  #ifdef K_ENABLED_LOG10_
+  strcpy(szKernels[num_kernels++], "log10_");
+  #endif
+  #ifdef K_ENABLED_LOGN_
+  strcpy(szKernels[num_kernels++], "logn_");
+  #endif
+  #ifdef K_ENABLED_MOD_
+  strcpy(szKernels[num_kernels++], "mod_");
+  #endif
+  #ifdef K_ENABLED_MULT_
+  strcpy(szKernels[num_kernels++], "mult_");
+  #endif
+  #ifdef K_ENABLED_NORMALIZE_
+  strcpy(szKernels[num_kernels++], "normalize_");
+  #endif
+  #ifdef K_ENABLED_POW_
+  strcpy(szKernels[num_kernels++], "pow_");
+  #endif
+  #ifdef K_ENABLED_POWB_
+  strcpy(szKernels[num_kernels++], "powb_");
+  #endif
+  #ifdef K_ENABLED_RECIPROCAL_
+  strcpy(szKernels[num_kernels++], "reciprocal_");
+  #endif
+  #ifdef K_ENABLED_REMAINDER_
+  strcpy(szKernels[num_kernels++], "remainder_");
+  #endif
+  #ifdef K_ENABLED_ROUND_
+  strcpy(szKernels[num_kernels++], "round_");
+  #endif
+  #ifdef K_ENABLED_RSQRT_
+  strcpy(szKernels[num_kernels++], "rsqrt_");
+  #endif
+  #ifdef K_ENABLED_SIGMOID_
+  strcpy(szKernels[num_kernels++], "sigmoid_");
+  #endif
+  #ifdef K_ENABLED_SIGN_
+  strcpy(szKernels[num_kernels++], "sign_");
+  #endif
+  #ifdef K_ENABLED_SIN_
+  strcpy(szKernels[num_kernels++], "sin_");
+  #endif
+  #ifdef K_ENABLED_SINH_
+  strcpy(szKernels[num_kernels++], "sinh_");
+  #endif
+  #ifdef K_ENABLED_SQR_
+  strcpy(szKernels[num_kernels++], "sqr_");
+  #endif
+  #ifdef K_ENABLED_SQRT_
+  strcpy(szKernels[num_kernels++], "sqrt_");
+  #endif
+  #ifdef K_ENABLED_TAN_
+  strcpy(szKernels[num_kernels++], "tan_");
+  #endif
+  #ifdef K_ENABLED_TANH_
+  strcpy(szKernels[num_kernels++], "tanh_");
+  #endif
+  #ifdef K_ENABLED_TRUNC_
+  strcpy(szKernels[num_kernels++], "trunc_");
+  #endif
+  #ifdef K_ENABLED_ADD
+  strcpy(szKernels[num_kernels++], "add");
+  #endif
+  #ifdef K_ENABLED_INC
+  strcpy(szKernels[num_kernels++], "inc");
+  #endif
+  #ifdef K_ENABLED_EL_DIV
+  strcpy(szKernels[num_kernels++], "el_div");
+  #endif
+  #ifdef K_ENABLED_EL_MULT
+  strcpy(szKernels[num_kernels++], "el_mult");
+  #endif
+  #ifdef K_ENABLED_SIGN2
+  strcpy(szKernels[num_kernels++], "sign2");
+  #endif
+  #ifdef K_ENABLED_SUM2D_ROWWISE
+  strcpy(szKernels[num_kernels++], "sum2d_rowwise");
+  #endif
+  #ifdef K_ENABLED_SUM2D_COLWISE
+  strcpy(szKernels[num_kernels++], "sum2d_colwise");
+  #endif
+  #ifdef K_ENABLED_MAX
+  strcpy(szKernels[num_kernels++], "max");
+  #endif
+  #ifdef K_ENABLED_SUM
+  strcpy(szKernels[num_kernels++], "sum");
+  #endif
+  #ifdef K_ENABLED_SUM_ABS
+  strcpy(szKernels[num_kernels++], "sum_abs");
+  #endif
+
+  // Metrics
+  #ifdef K_ENABLED_ACCURACY
+  strcpy(szKernels[num_kernels++], "accuracy");
+  #endif
+
+  // Pool
+  #ifdef K_ENABLED_MPOOL2D
+  strcpy(szKernels[num_kernels++], "mpool2d");
+  #endif
+  #ifdef K_ENABLED_MPOOL2D_BACK
+  strcpy(szKernels[num_kernels++], "mpool2d_back");
+  #endif
+  #ifdef K_ENABLED_AVGPOOL2D
+  strcpy(szKernels[num_kernels++], "avgpool2d");
+  #endif
+  #ifdef K_ENABLED_AVGPOOL2D_BACK
+  strcpy(szKernels[num_kernels++], "avgpool2d_back");
+  #endif
+
+  // Reduction
+  #ifdef K_ENABLED_REDUCE
+  strcpy(szKernels[num_kernels++], "reduce");
+  #endif
+  #ifdef K_ENABLED_REDUCE2
+  strcpy(szKernels[num_kernels++], "reduce2");
+  #endif
+  #ifdef K_ENABLED_REDUCE_OP
+  strcpy(szKernels[num_kernels++], "reduce_op");
+  #endif
+  #ifdef K_ENABLED_OPT2
+  strcpy(szKernels[num_kernels++], "opt2");
+  #endif
+  #ifdef K_ENABLED_REDUCE_SUM2D
+  strcpy(szKernels[num_kernels++], "reduce_sum2d");
+  #endif
+  #ifdef K_ENABLED_REDUCTION
+  strcpy(szKernels[num_kernels++], "reduction");
+  #endif
+  #ifdef K_ENABLED_REDUCTION_BACK
+  strcpy(szKernels[num_kernels++], "reduction_back");
+  #endif
+
+  // Tensor_nn
+  #ifdef K_ENABLED_REPEAT_NN
+  strcpy(szKernels[num_kernels++], "repeat_nn");
+  #endif
+  #ifdef K_ENABLED_D_REPEAT_NN
+  strcpy(szKernels[num_kernels++], "d_repeat_nn");
+  #endif
 
   // first part of the makefile
   printf("CXXFLAGS =-std=c++11 -g\n");
