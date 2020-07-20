a42aa208729aa6cfab51777e62913226c2156526
Jorga20j <jorga20j@upv.gap.es>
2020-07-20 08:31:23 +0000

    reduce_sum2d pre-optimized

--- fpga_kernels/kernel_reduce_sum2d.cpp
+++ fpga_kernels/kernel_reduce_sum2d.cpp
@@ -2,10 +2,13 @@
 #include <stdio.h>
 extern "C" {
 
+
+#define BUFFER_SIZE 8
+
 void k_reduce_sum2d(float *A, float *B, int Ashape0, int Ashape1, int axis, int incB) {
 
-  #pragma HLS INTERFACE m_axi port=A offset=slave bundle=gmem
-  #pragma HLS INTERFACE m_axi port=B offset=slave bundle=gmem
+  #pragma HLS INTERFACE m_axi port=A offset=slave bundle=gmem1
+  #pragma HLS INTERFACE m_axi port=B offset=slave bundle=gmem2
   #pragma HLS INTERFACE s_axilite port=A  bundle=control
   #pragma HLS INTERFACE s_axilite port=B  bundle=control
   #pragma HLS INTERFACE s_axilite port=Ashape0 bundle=control
@@ -14,25 +17,87 @@ void k_reduce_sum2d(float *A, float *B, int Ashape0, int Ashape1, int axis, int 
   #pragma HLS INTERFACE s_axilite port=incB bundle=control
   #pragma HLS INTERFACE s_axilite port=return bundle=control
 
+  //Local variables
+  float Local_A[BUFFER_SIZE][BUFFER_SIZE];
+  float Local_B[BUFFER_SIZE];
+  int block_row = ceil(float(Ashape0)/BUFFER_SIZE);
+  int block_col = ceil(float(Ashape1)/BUFFER_SIZE);
+  int Bshape;
 
-    if (axis==0){
-        if (!incB) for(int i=0;i<Ashape1;++i) {B[i]=0;}
-        int p=0;
-        for(int i=0;i<Ashape0;++i) {
-          for(int j=0;j<Ashape1;++j,p++){
-              B[j]+=A[p];
-          }
-        }
-     }
-     else{
-        if (!incB) for(int i=0;i<Ashape0;++i){B[i]=0;}
-        int p=0;
-        for(int i=0;i<Ashape0;++i) {
-          for(int j=0;j<Ashape1;++j,p++){
-                B[i]+=A[p];
-          }
-        }
+
+  //Checking variables
+  if(!incB){
+    for(int i = 0; i<Ashape1; i++){
+      B[i]=0.0;
     }
-}
+  }
 
+  if(axis){
+    Bshape = Ashape0;
+  }
+  else{
+    Bshape = Ashape1;
+  }
+
+  // Loop tiling
+  for(int block_i = 0; block_i < block_row; block_i++ ){
+    for(int block_j = 0; block_j < block_col; block_j++){
+
+      // Read A and init Local_A
+      for(int i = 0; i < BUFFER_SIZE; i++){
+        for(int j = 0; j < BUFFER_SIZE; j++){
+
+          int ii = i + (block_i*BUFFER_SIZE); // Absolute index i
+          int jj = j + (block_j*BUFFER_SIZE); // Absolute index j
+
+          // Checking matrix limits
+          if((ii >= Ashape0) || (jj >= Ashape1)){
+            Local_A[i][j] = 0;
+          }
+          else{
+            Local_A[i][j] = A[jj + Ashape1*ii];
+          }
+
+        }
+      }
+
+      //Init Local_B
+      for(int j=0; j<BUFFER_SIZE; j++){
+        Local_B[j] = 0.0;
+      }
+
+      //Compute
+      for(int i=0;i<BUFFER_SIZE;i++){
+        for(int j=0;j<+BUFFER_SIZE;j++){
+
+          if(axis){
+            Local_B[i] += Local_A[i][j];
+          }
+          else{
+            Local_B[j] += Local_A[i][j];
+          }
+
+        }
+      }
+
+      // Write B from Local_B
+      for(int j=0; j< BUFFER_SIZE; j++){
+        if(axis){
+          if(((block_i*BUFFER_SIZE) + j) < Bshape){
+            B[(j+(block_i*BUFFER_SIZE))] += Local_B[j];
+            }
+        }
+        else{
+          if(((block_j*BUFFER_SIZE) + j) < Bshape){
+            B[(j+(block_j*BUFFER_SIZE))] += Local_B[j];
+          }
+        }
+
+      }
+
+
+    }
+  } //End loop tiling
+
+}
 } // end extern "C"
