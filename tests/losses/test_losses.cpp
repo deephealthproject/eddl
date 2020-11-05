615613a0daee57bcffd0b5c7839a1a3f9e46eb15
salvacarrion <salva.carrion@outlook.com>
2020-11-05 14:17:33 +0100

    Add test for BinaryCrossEntropy

--- tests/losses/test_losses.cpp
+++ tests/losses/test_losses.cpp
@@ -19,26 +19,26 @@ TEST(NetTestSuite, losses_categorical_cross_entropy){
 
     auto loss = LCategoricalCrossEntropy();
 
-    Tensor* t1_y_pred = new Tensor({0.7, 0.2, 0.1,
+    Tensor* t1_y_true_pred = new Tensor({0.7, 0.2, 0.1,
                                           0.3, 0.5, 0.2}, {2, 3});
-    Tensor* t1_y = new Tensor({1.0, 0.0, 0.0,
+    Tensor* t1_y_true = new Tensor({1.0, 0.0, 0.0,
                                     0.0, 1.0, 0.0}, {2, 3});
 
     // Compute loss
-    float value = loss.value(t1_y, t1_y_pred);
+    float value = loss.value(t1_y_true, t1_y_true_pred);
     ASSERT_NEAR(value, 0.524911046f, 10e-4f);
 
 
     // Compute delta
-    Tensor* t1_delta = Tensor::zeros_like(t1_y);
+    Tensor* t1_delta = Tensor::zeros_like(t1_y_true);
     Tensor* t1_delta_ref = new Tensor({-1.4285f, 0.0, 0.0,
                                            0.0, -2.0000f, 0.0}, {2, 3});
-    loss.delta(t1_y, t1_y_pred, t1_delta);
+    loss.delta(t1_y_true, t1_y_true_pred, t1_delta);
     ASSERT_TRUE(Tensor::equivalent(t1_delta_ref, t1_delta, 10e-4));
 
     // Deletes
-    delete t1_y_pred;
-    delete t1_y;
+    delete t1_y_true_pred;
+    delete t1_y_true;
     delete t1_delta;
     delete t1_delta_ref;
 
@@ -82,3 +82,68 @@ TEST(NetTestSuite, losses_categorical_cross_entropy){
 #endif
 }
 
+
+TEST(NetTestSuite, losses_binary_cross_entropy){
+
+    auto loss = LBinaryCrossEntropy();
+
+    Tensor* t1_y_true_pred = new Tensor({0.7, 0.3, 0.9, 0.1, 0.6}, {5, 1});
+    Tensor* t1_y_true = new Tensor({1.0, 1.0, 1.0, 0.0, 1.0}, {5, 1});
+
+    // Compute loss
+    float value = loss.value(t1_y_true, t1_y_true_pred);
+    ASSERT_NEAR(value, 0.4564, 10e-4f);
+
+
+    // Compute delta
+    Tensor* t1_delta = Tensor::zeros_like(t1_y_true);
+    Tensor* t1_delta_ref = new Tensor({-1.4285, -3.3333, -1.1111, 1.1111, -1.6666,}, {5, 1}); 
+    loss.delta(t1_y_true, t1_y_true_pred, t1_delta);
+    ASSERT_TRUE(Tensor::equivalent(t1_delta_ref, t1_delta, 10e-4));
+
+    // Deletes
+    delete t1_y_true_pred;
+    delete t1_y_true;
+    delete t1_delta;
+    delete t1_delta_ref;
+
+    // Test GPU
+#ifdef cGPU
+    // Test: Loss value
+    // Generate default predictions
+    int rows = 1000;
+    Tensor* t_cpu_y_pred = Tensor::randu({rows}); // Default values between 0 and 1
+    t_cpu_y_pred->clamp_(0.0f, 1.0f); // Clamp between 0.0 a 1 (just in case)
+    Tensor* t_gpu_y_pred = t_cpu_y_pred->clone(); t_gpu_y_pred->toGPU();
+
+    // Generate one-hots
+    Tensor* t_cpu_y_true = Tensor::randu({rows}); // Default values between 0 and 1
+    t_cpu_y_true->greater(0.5f); // // Force values to be either 0 and 1
+    Tensor* t_gpu_y_true = t_cpu_y_true->clone(); t_gpu_y_true->toGPU();
+
+    // Compute loss
+    float cpu_loss = loss.value(t_cpu_y_true, t_cpu_y_pred);
+    float gpu_loss = loss.value(t_gpu_y_true, t_gpu_y_pred);
+    ASSERT_NEAR(cpu_loss-gpu_loss, 0.0f, 10e-4f);
+
+    // Test: Deltas
+    // Generate matrices
+    Tensor* t_cpu_delta = Tensor::zeros_like(t_cpu_y_pred);
+    Tensor* t_gpu_delta = t_cpu_delta->clone(); t_gpu_delta->toGPU();
+
+    // Compute deltas
+    loss.delta(t_cpu_y_true, t_cpu_y_pred, t_cpu_delta);
+    loss.delta(t_gpu_y_true, t_gpu_y_pred, t_gpu_delta);
+
+    t_gpu_delta->toCPU();  // Send to CPU
+    ASSERT_TRUE(Tensor::equivalent(t_cpu_delta, t_gpu_delta, 10e-4));
+
+    // Deletes
+    delete t_cpu_y_pred;
+    delete t_gpu_y_pred;
+    delete t_cpu_y_true;
+    delete t_gpu_y_true;
+    delete t_cpu_delta;
+    delete t_gpu_delta;
+#endif
+}
