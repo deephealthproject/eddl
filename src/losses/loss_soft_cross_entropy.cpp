55c4a9bbe68def6dcda9d353bedd86c2e86bdf28
salvacarrion <salva.carrion@outlook.com>
2020-11-11 14:34:19 +0100

    Update SoftCrossEntropy

--- src/losses/loss_soft_cross_entropy.cpp
+++ src/losses/loss_soft_cross_entropy.cpp
@@ -24,25 +24,11 @@ LSoftCrossEntropy::LSoftCrossEntropy() : Loss("soft_cross_entropy"){
 
 void LSoftCrossEntropy::delta(Tensor *T, Tensor *Y, Tensor *D) {
     Tensor::add(-1.0, T, 1.0, Y, D, 0);
-    D->div_(D->shape[0]);
 }
 
 float LSoftCrossEntropy::value(Tensor *T, Tensor *Y) {
-    float f;
-    Tensor *aux1;
-    int size=T->size/T->shape[0];  // batch is divided in print_loss
-
-    aux1 = new Tensor(T->getShape(), T->device);
-    tensorNN::cent(T, Y, aux1);
-    //f = aux1->sum()/size;
-    // corss-entropy use to be used with one-hot vectors
-    // dividing by size of vector gives too small values when
-    // for instance nuber of classes is too large.
-    f = aux1->sum();
-
-    delete aux1;
-
-    return f;
+    float loss_value = tensorNN::categorical_cross_entropy(T, Y);
+    return loss_value;
 }
 Loss* LSoftCrossEntropy::clone()
 {
