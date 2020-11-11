4c504d17bbf520f1ff4a9c36ddf59e2493364f38
Michele Cancilla <cancilla.michele@gmail.com>
2020-11-11 18:20:46 +0100

    Fix Windows errors related to "sys/time.h"

---
+++ src/profiling.cpp
@@ -0,0 +1,23 @@
+/*
+* EDDL Library - European Distributed Deep Learning Library.
+* Version: 0.7
+* copyright (c) 2020, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
+* Date: April 2020
+* Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
+* All rights reserved
+*/
+
+#include "eddl/profiling.h"
+
+#ifdef EDDL_WINDOWS
+int gettimeofday(struct timeval* tp, struct timezone* tzp)
+{
+    namespace sc = std::chrono;
+    sc::system_clock::duration d = sc::system_clock::now().time_since_epoch();
+    sc::seconds s = sc::duration_cast<sc::seconds>(d);
+    tp->tv_sec = s.count();
+    tp->tv_usec = sc::duration_cast<sc::microseconds>(d - s).count();
+
+    return 0;
+}
+#endif
