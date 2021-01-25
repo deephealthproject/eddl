42b6ff03b6b5faaa9f25f5c444bd34b92dd37f88
adcastel <adcastel@uji.es>
2021-01-25 11:16:44 +0100

    max_pool backward added

--- src/hardware/gpu/nn/gpu_pool.cu
+++ src/hardware/gpu/nn/gpu_pool.cu
@@ -44,11 +44,18 @@ void gpu_mpool2D(PoolDescriptor *D){
 void gpu_mpool2D_back(PoolDescriptor *D){
     int device=D->I->gpu_device;
     cudaSetDevice(device);
-
+#ifndef cCUDNN
     setDims(D->D)
     maxpool2d_back<<<dimGrid,dimBlock>>>(D->D->ptr, D->ID->ptr, D->I->shape[0],D->ir,D->ic,D->iz,D->kr,D->kc,D->O->ptr,D->r,D->c,D->z, D->sr,D->sc,D->padrt,D->padrb,D->padcl,D->padcr, D->indX->ptr, D->indY->ptr);
 
     check_cuda(cudaDeviceSynchronize(),"gpu_mpool_back");
+#else
+    float alpha=1.0;
+    float beta=0.0;
+    check_cudnn(cudnnPoolingBackward(D->cudnn_handle, D->poolingDesc, &alpha, D->yDesc, D->O->ptr,
+                                     D->yDesc, D->D->ptr, D->xDesc, D->I->ptr,
+                                     &beta, D->xDesc, D->ID->ptr));
+#endif
 }
 
 
