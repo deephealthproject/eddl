f4a7fe9908dc37f36d0a174c4b0185c619c070b9
salvacarrion <salva.carrion@outlook.com>
2021-01-26 12:59:34 +0100

    Add gpu_mpool3D (tensor)

--- include/eddl/hardware/gpu/nn/gpu_tensor_nn.h
+++ include/eddl/hardware/gpu/nn/gpu_tensor_nn.h
@@ -94,6 +94,10 @@ void gpu_conv3D_back(ConvolDescriptor3D *D);
 void gpu_mpool2D(PoolDescriptor *D);
 void gpu_mpool2D_back(PoolDescriptor *D);
 
+// MaxPool3D
+void gpu_mpool3D(PoolDescriptor3D *D);
+void gpu_mpool3D_back(PoolDescriptor3D *D);
+
 // AvgPool
 void gpu_avgpool2D(PoolDescriptor *D);
 void gpu_avgpool2D_back(PoolDescriptor *D);
