ca8bfabeba509cec0acc0affdf1489bcd816ed38
salvacarrion <salva.carrion@outlook.com>
2021-01-28 15:31:37 +0100

    Fix dummy error in conv test

--- tests/layers/conv/test_conv2d.cpp
+++ tests/layers/conv/test_conv2d.cpp
@@ -6,111 +6,128 @@
 
 
 
-//TEST(Conv2DTestSuite, conv2d_custom)
-//{
-//    // Image (force padding manually, I don't want surprises)
-//    auto *ptr_img = new float[3*7*7]{
-//                                    0, 0, 0, 0, 0, 0, 0,
-//                                    0, 0, 2, 1, 0, 1, 0,
-//                                    0, 2, 2, 1, 0, 0, 0,
-//                                    0, 0, 0, 2, 0, 1, 0,
-//                                    0, 0, 2, 1, 2, 0, 0,
-//                                    0, 2, 2, 0, 2, 0, 0,
-//                                    0, 0, 0, 0, 0, 0, 0,
-//
-//                                    0, 0, 0, 0, 0, 0, 0,
-//                                    0, 2, 0, 2, 1, 2, 0,
-//                                    0, 0, 2, 0, 1, 0, 0,
-//                                    0, 1, 2, 0, 2, 2, 0,
-//                                    0, 0, 0, 2, 1, 2, 0,
-//                                    0, 1, 1, 1, 1, 1, 0,
-//                                    0, 0, 0, 0, 0, 0, 0,
-//
-//                                    0, 0, 0, 0, 0, 0, 0,
-//                                    0, 1, 1, 2, 0, 0, 0,
-//                                    0, 2, 0, 2, 0, 0, 0,
-//                                    0, 2, 1, 1, 2, 1, 0,
-//                                    0, 0, 2, 0, 1, 2, 0,
-//                                    0, 1, 1, 1, 0, 1, 0,
-//                                    0, 0, 0, 0, 0, 0, 0,
-//    };
-//    auto* t_image = new Tensor({1, 1, 7, 7}, ptr_img, DEV_CPU);
-////    t_image->toGPU();
-//
-//    // Forward
-//    auto *ptr_fwrd = new float[2*3*3]{-3, -2, -2,
-//                                      3, -9, -7,
-//                                      -2, -1, -2,
-//
-//                                      -1, 1, 2,
-//                                      -3, 3, 5,
-//                                      3, 1, 2};
-//    auto* t_fwrd = new Tensor({1, 2, 3, 3}, ptr_fwrd, DEV_CPU);
-////    t_fwrd->toGPU();
-//
-//
-////    // backward
-////    auto *ptr_bwrd = new float[5*5]{1.00, 1.00, 1.00, 1.00, 0.00,
-////                                    1.00, 1.00, 1.00, 1.00, 0.00,
-////                                    1.00, 1.00, 1.00, 1.00, 0.00,
-////                                    1.00, 1.00, 1.00, 1.00, 0.00,
-////                                    0.00, 0.00, 0.00, 0.00, 0.00};
-////    auto* t_bwrd = new Tensor({1, 1, 5, 5}, ptr_bwrd, DEV_CPU);
-//
-//    // Kernels (=> w0(3x 3x3), w1(3x 3x3))
-//    auto *ptr_kernels = new float[2*3*3*3]{
-//                                            -1,  1,  0,
-//                                             1, -1,  0,
-//                                            -1, -1, -1,
-//
-//                                            -1,  0, 0,
-//                                            -1,  0, 1,
-//                                            -1, -1, 0,
-//
-//                                            -1, 0, -1,
-//                                             0, 0,  0,
-//                                             1, 0,  0,
-//
-//                                             0, -1, 0,
-//                                             -1, 0, 1,
-//                                             1, 0, -1,
-//
-//                                             0, 0, -1,
-//                                             1, 0,  0,
-//                                             1, 0, -1,
-//
-//                                             1, 1, 0,
-//                                             0, 0, 1,
-//                                             0, 0, 0,
-//    };
-//
-//    // Biases (One per kernel)
-//    auto *ptr_bias = new float[2]{1.0, 0.0};
-//
-//    // Operation
-//    auto *cd = new ConvolDescriptor(2, {3, 3}, {2, 2}, "none", true);
-//    cd->build(t_image);
-//    cd->K = new Tensor({1, 3, 3, 3}, ptr_kernels, DEV_CPU);
-//    cd->bias = new Tensor({1}, ptr_bias, DEV_CPU); //Tensor::zeros(cd->bias->getShape());
-//    cd->ID = Tensor::zeros(cd->I->getShape());
-//    cd->D = Tensor::ones(cd->O->getShape());
-//
-////    cd->K->toGPU();
-////    cd->bias->toGPU();
-////    cd->ID->toGPU();
-////    cd->D->toGPU();
-//
-//    // Forward
-//    tensorNN::Conv2D(cd);
-//    cd->O->toCPU(); cd->O->print(2, true);
-//    ASSERT_TRUE((bool) Tensor::equivalent(t_fwrd, cd->O, 10e-5f));
-//
-////    // Backward
-////    tensorNN::Conv2D_back(cd);
-////    ASSERT_TRUE((bool) Tensor::equivalent(t_bwrd, cd->ID, 10e-5f));
-//    int asd = 3;
-//
-//}
+TEST(Conv2DTestSuite, conv2d_custom)
+{
+    // Image (force padding manually, I don't want surprises)
+    auto *ptr_img = new float[3*7*7]{
+                                    0, 0, 0, 0, 0, 0, 0,
+                                    0, 0, 2, 1, 0, 1, 0,
+                                    0, 2, 2, 1, 0, 0, 0,
+                                    0, 0, 0, 2, 0, 1, 0,
+                                    0, 0, 2, 1, 2, 0, 0,
+                                    0, 2, 2, 0, 2, 0, 0,
+                                    0, 0, 0, 0, 0, 0, 0,
+
+                                    0, 0, 0, 0, 0, 0, 0,
+                                    0, 2, 0, 2, 1, 2, 0,
+                                    0, 0, 2, 0, 1, 0, 0,
+                                    0, 1, 2, 0, 2, 2, 0,
+                                    0, 0, 0, 2, 1, 2, 0,
+                                    0, 1, 1, 1, 1, 1, 0,
+                                    0, 0, 0, 0, 0, 0, 0,
+
+                                    0, 0, 0, 0, 0, 0, 0,
+                                    0, 1, 1, 2, 0, 0, 0,
+                                    0, 2, 0, 2, 0, 0, 0,
+                                    0, 2, 1, 1, 2, 1, 0,
+                                    0, 0, 2, 0, 1, 2, 0,
+                                    0, 1, 1, 1, 0, 1, 0,
+                                    0, 0, 0, 0, 0, 0, 0,
+    };
+    auto* t_image = new Tensor({1, 3, 7, 7}, ptr_img, DEV_CPU);
+//    t_image->toGPU();
+
+    // Forward
+    auto *ptr_fwrd = new float[2*3*3]{-3, -2, -2,
+                                      3, -9, -7,
+                                      -2, -1, -2,
+
+                                      -1, 1, 2,
+                                      -3, 3, 5,
+                                      3, 1, 2};
+    auto* t_fwrd = new Tensor({1, 2, 3, 3}, ptr_fwrd, DEV_CPU);
+//    t_fwrd->toGPU();
+
+
+    // backward
+    auto *ptr_bwrd = new float[3*7*7]{-1.00, 0.00, -1.00, 0.00, -1.00, 0.00, 0.00,
+                                    0.00, -1.00, 1.00, -1.00, 1.00, -1.00, 1.00,
+                                    -1.00, -1.00, -3.00, -1.00, -3.00, -1.00, -2.00,
+                                    0.00, -1.00, 1.00, -1.00, 1.00, -1.00, 1.00,
+                                    -1.00, -1.00, -3.00, -1.00, -3.00, -1.00, -2.00,
+                                    0.00, -1.00, 1.00, -1.00, 1.00, -1.00, 1.00,
+                                    0.00, -1.00, -2.00, -1.00, -2.00, -1.00, -2.00,
+                                    -1.00, 0.00, -2.00, 0.00, -2.00, 0.00, -1.00,
+                                    0.00, 0.00, 1.00, 0.00, 1.00, 0.00, 1.00, -1.00,
+                                    -1.00, -3.00, -1.00, -3.00, -1.00, -2.00, 0.00,
+                                    0.00, 1.00, 0.00, 1.00, 0.00, 1.00, -1.00, -1.00,
+                                    -3.00, -1.00, -3.00, -1.00, -2.00, 0.00, 0.00,
+                                    1.00, 0.00, 1.00, 0.00, 1.00, 0.00, -1.00, -1.00,
+                                    -1.00, -1.00, -1.00, -1.00, 0.00, 1.00, -1.00,
+                                    1.00, -1.00, 1.00, -1.00, 0.00, 0.00, 1.00, 0.00,
+                                    1.00, 0.00, 1.00, 1.00, 1.00, 0.00, 1.00, 0.00,
+                                    1.00, -1.00, 0.00, 0.00, 1.00, 0.00, 1.00, 0.00,
+                                    1.00, 1.00, 1.00, 0.00, 1.00, 0.00, 1.00, -1.00,
+                                    0.00, 0.00, 1.00, 0.00, 1.00, 0.00, 1.00, 1.00,
+                                    0.00, 1.00, 0.00, 1.00, 0.00, 0.00,
+    };
+    auto* t_bwrd = new Tensor({1, 3, 7, 7}, ptr_bwrd, DEV_CPU);
+//    t_bwrd->toGPU();
+
+    // Kernels (=> w0(3x 3x3), w1(3x 3x3))
+    auto *ptr_kernels = new float[2*3*3*3]{
+                                            -1,  1,  0,
+                                             1, -1,  0,
+                                            -1, -1, -1,
+
+                                            -1,  0, 0,
+                                            -1,  0, 1,
+                                            -1, -1, 0,
+
+                                            -1, 0, -1,
+                                             0, 0,  0,
+                                             1, 0,  0,
+
+
+                                             0, -1, 0,
+                                             -1, 0, 1,
+                                             1, 0, -1,
+
+                                             0, 0, -1,
+                                             1, 0,  0,
+                                             1, 0, -1,
+
+                                             1, 1, 0,
+                                             0, 0, 1,
+                                             0, 0, 0,
+    };
+
+    // Biases (One per kernel)
+    auto *ptr_bias = new float[2]{1.0, 0.0};
+
+    // Operation
+    auto *cd = new ConvolDescriptor(2, {3, 3}, {2, 2}, "none", true);
+    cd->build(t_image);
+    cd->K = new Tensor({2, 3, 3, 3}, ptr_kernels, DEV_CPU);
+    cd->bias = new Tensor({2}, ptr_bias, DEV_CPU); //Tensor::zeros(cd->bias->getShape());
+    cd->ID = Tensor::zeros(cd->I->getShape());
+    cd->D = Tensor::ones(cd->O->getShape());
+
+//    cd->K->toGPU();
+//    cd->bias->toGPU();
+//    cd->ID->toGPU();
+//    cd->D->toGPU();
+
+    // Forward
+    tensorNN::Conv2D(cd);
+//    cd->O->toCPU(); cd->O->print(1, true);
+    ASSERT_TRUE((bool) Tensor::equivalent(t_fwrd, cd->O, 10e-5f));
+
+    // Backward
+    tensorNN::Conv2D_back(cd);
+//    cd->ID->toCPU(); cd->ID->print(2, true);
+    ASSERT_TRUE((bool) Tensor::equivalent(t_bwrd, cd->ID, 10e-5f));
+}
 
 TEST(Conv2DTestSuite, conv2d_k2x2_s2x2_pad_valid)
 {
