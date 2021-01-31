23f9f5a1de502ec201212861a2396780e9a6acb9
Jon Ander Gómez <jon@dsic.upv.es>
2021-01-31 10:10:26 +0100

    Mnist examples updated to delete/free all objects + memory leaks in conv1D removed

--- examples/nn/1_mnist/7_mnist_conv.cpp
+++ examples/nn/1_mnist/7_mnist_conv.cpp
@@ -23,12 +23,18 @@ using namespace eddl;
 //////////////////////////////////
 
 int main(int argc, char **argv) {
+    bool testing = false;
+    bool use_cpu = false;
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "--testing") == 0) testing = true;
+        else if (strcmp(argv[i], "--cpu") == 0) use_cpu = true;
+    }
 
     // Download mnist
     download_mnist();
 
     // Settings
-    int epochs = 10;
+    int epochs = (testing) ? 2 : 10;
     int batch_size = 100;
     int num_classes = 10;
 
@@ -50,16 +56,22 @@ int main(int argc, char **argv) {
     // dot from graphviz should be installed:
     plot(net, "model.pdf");
 
+    compserv cs = nullptr;
+    if (use_cpu) {
+        cs = CS_CPU();
+    } else {
+        cs = CS_GPU({1}, "low_mem"); // one GPU
+        // cs = CS_GPU({1,1},100); // two GPU with weight sync every 100 batches
+        // cs = CS_CPU();
+        // cs = CS_FPGA({1});
+    }
+
     // Build model
     build(net,
           rmsprop(0.001), // Optimizer
           {"softmax_cross_entropy"}, // Losses
           {"categorical_accuracy"}, // Metrics
-          //CS_GPU({1}) // one GPU
-          //CS_GPU({1,1},100) // two GPU with weight sync every 100 batches
-          CS_CPU()
-	  //CS_FPGA({1})
-    );
+          cs);
 
     // View model
     summary(net);
@@ -69,6 +81,24 @@ int main(int argc, char **argv) {
     Tensor* y_train = Tensor::load("mnist_trY.bin");
     Tensor* x_test = Tensor::load("mnist_tsX.bin");
     Tensor* y_test = Tensor::load("mnist_tsY.bin");
+
+    if (testing) {
+        std::string _range_ = "0:" + std::to_string(2 * batch_size);
+        Tensor* x_mini_train = x_train->select({_range_, ":"});
+        Tensor* y_mini_train = y_train->select({_range_, ":"});
+        Tensor* x_mini_test  = x_test->select({_range_, ":"});
+        Tensor* y_mini_test  = y_test->select({_range_, ":"});
+
+        delete x_train;
+        delete y_train;
+        delete x_test;
+        delete y_test;
+
+        x_train = x_mini_train;
+        y_train = y_mini_train;
+        x_test  = x_mini_test;
+        y_test  = y_mini_test;
+    }
 
     // Preprocessing
     x_train->div_(255.0f);
