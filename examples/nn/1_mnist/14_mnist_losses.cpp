7246a354da10938d66827db95d0c072e5cc4c5a7
salvacarrion <salva.carrion@outlook.com>
2021-02-03 18:53:55 +0100

    Update version

--- examples/nn/1_mnist/14_mnist_losses.cpp
+++ examples/nn/1_mnist/14_mnist_losses.cpp
@@ -1,6 +1,6 @@
 /*
 * EDDL Library - European Distributed Deep Learning Library.
-* Version: 0.8
+* Version: 0.9
 * copyright (c) 2019, Universidad Politécnica de Valencia (UPV), PRHLT Research Centre
 * Date: October 2019
 * Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
