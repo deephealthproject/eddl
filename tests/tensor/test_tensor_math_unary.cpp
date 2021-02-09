a72f7aafdb324f1bf4fc0385aca02e6849a16cb8
salvacarrion <salva.carrion@outlook.com>
2021-02-09 14:25:27 +0100

    Fix mode test

--- tests/tensor/test_tensor_math_unary.cpp
+++ tests/tensor/test_tensor_math_unary.cpp
@@ -1867,12 +1867,10 @@ TEST(TensorTestSuite, tensor_math_unary_mode){
 
     // Test GPU
 #ifdef cGPU
-    Tensor* t1_cpu = Tensor::randn({10}); t1_cpu->round_();
+    Tensor* t1_cpu = Tensor::randn({1001}); t1_cpu->ceil_();  // Must be an odd size to avoid things like: (2, 2, 3, 3)
     Tensor* t1_gpu = t1_cpu->clone(); t1_gpu->toGPU();
     int t1_cpu_mode = t1_cpu->mode();
     int t1_gpu_mode = t1_gpu->mode(); t1_gpu->toCPU();
-//    t1_cpu->print();
-//    t1_gpu->print();
     ASSERT_EQ(t1_cpu_mode, t1_gpu_mode);
 
     delete t1_cpu;
