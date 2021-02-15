d483cf77e49f93c75e04d41119ab5adce8b774f1
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-02-15 13:21:08 +0100

    Header doc strings for Optimizer and Compserv onnx functions

--- include/eddl/serialization/onnx/eddl_onnx.h
+++ include/eddl/serialization/onnx/eddl_onnx.h
@@ -52,6 +52,14 @@ Net *import_net_from_onnx_string(string *model_string, int mem = 0);
 // Optimizer loaders
 //-------------------
 
+/**
+ * @brief Creates an Optimizer from the definition provided in an ONNX file. The
+ *        ONNX will provide the Optimizer type and its attributes.
+ *
+ * @param path Path to the file where the Optimizer configuration is saved
+ *
+ * @return Optimizer*
+ */
 Optimizer *import_optimizer_from_onnx_file(string path);
 
 Optimizer *import_optimizer_from_onnx_pointer(void *serialized_optimizer, size_t optimizer_size);
@@ -62,6 +70,14 @@ Optimizer *import_optimizer_from_onnx_string(string *optimizer_string);
 // Computing Service loaders
 //---------------------------
 
+/**
+ * @brief Creates a CompServ from the definition provided in an ONNX file. The
+ *        ONNX will provide the CompServ type and its configuration.
+ *
+ * @param path Path to the file where the CompServ configuration is saved
+ *
+ * @return CompServ*
+ */
 CompServ *import_compserv_from_onnx_file(string path);
 
 CompServ *import_compserv_from_onnx_pointer(void *serialized_cs, size_t cs_size);
@@ -94,6 +110,15 @@ string *serialize_net_to_onnx_string(Net *net, bool gradients = false);
 // Optimizer exporters
 //---------------------
 
+/**
+ * @brief Saves the configuration of an optimizer using the ONNX format. It will contain
+ *        the Optimizer type and attributes like learning rate, momentum, weight decay, etc.
+ *
+ * @param optimizer Optimizer to be saved
+ * @param path Path to the file where the Optimizer configuration will be saved
+ *
+ * @return (void)
+ */
 void save_optimizer_to_onnx_file(Optimizer *optimizer, string path);
 
 size_t serialize_optimizer_to_onnx_pointer(Optimizer *optimizer, void *&serialized_optimizer);
@@ -104,6 +129,15 @@ string *serialize_optimizer_to_onnx_string(Optimizer *optimizer);
 // Computing Service exporters
 //-----------------------------
 
+/**
+ * @brief Saves the configuration of a computing service using the ONNX format. It will 
+ *        contain the computing service type and configuration of devices.
+ *
+ * @param cs CompServ to be saved
+ * @param path Path to the file where the computing service configuration will be saved
+ *
+ * @return (void)
+ */
 void save_compserv_to_onnx_file(CompServ *cs, string path);
 
 size_t serialize_compserv_to_onnx_pointer(CompServ *cs, void *&serialized_cs);
