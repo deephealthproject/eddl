a13106883260edaa13d1ad7ca71f55c467d2416e
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-02-24 13:40:30 +0100

    Fix to compile with BUILD_PROTOBUF=OFF

--- include/eddl/serialization/onnx/utils_onnx.h
+++ include/eddl/serialization/onnx/utils_onnx.h
@@ -1,25 +1,9 @@
 #ifndef EDDL_UTILS_ONNX_H
 #define EDDL_UTILS_ONNX_H
 #include "eddl/net/net.h"
+
+#if defined(cPROTO)
 #include "eddl/serialization/onnx/onnx.pb.h"
-
-#define COPY_FROM_VECTOR_PTR_TO_TENSOR(v, t) (copy((v)->begin(), (v)->end(), t->ptr))
-#define COPY_FROM_VECTOR_PTR_TO_FLOAT_PTR(v, ptr) (copy((v)->begin(), (v)->end(), ptr))
-
-enum LOG_LEVEL {
-  TRACE = 0,
-  DEBUG = 1,
-  INFO = 2,
-  WARN = 3,
-  ERROR = 4,
-  NO_LOGS = 5
-};
-
-// If the actual_log_level is <= than string_log_level prints log
-void log_string(string log, LOG_LEVEL actual_log_level, LOG_LEVEL string_log_level);
-
-// Return a new vector of ints from the float vector passed as a parameter
-std::vector<int> vf2vi(const std::vector<float> &vf);
 
 // Parses the values of the onnx tensor to a c++ vector of that type
 vector<float> parseTensorValues(onnx::TensorProto t);
@@ -42,6 +26,26 @@ bool TryConvertingTensorRawValues(const onnx::TensorProto &onnx_tensor, vector<T
   memcpy(target_ptr, src_ptr, raw_size);
   return true;
 }
+
+#endif // defined(cPROTO)
+
+#define COPY_FROM_VECTOR_PTR_TO_TENSOR(v, t) (copy((v)->begin(), (v)->end(), t->ptr))
+#define COPY_FROM_VECTOR_PTR_TO_FLOAT_PTR(v, ptr) (copy((v)->begin(), (v)->end(), ptr))
+
+enum LOG_LEVEL {
+  TRACE = 0,
+  DEBUG = 1,
+  INFO = 2,
+  WARN = 3,
+  ERROR = 4,
+  NO_LOGS = 5
+};
+
+// If the actual_log_level is <= than string_log_level prints log
+void log_string(string log, LOG_LEVEL actual_log_level, LOG_LEVEL string_log_level);
+
+// Return a new vector of ints from the float vector passed as a parameter
+std::vector<int> vf2vi(const std::vector<float> &vf);
 
 // Synchronize the weights of the snets with the original net
 // Also synchronizes the accumulated gradients if specified
