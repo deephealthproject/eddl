259e687de75fb4b58b97ed01711875d5ae9aef81
salvacarrion <salva.carrion@outlook.com>
2021-03-22 11:05:08 +0100

    Fix scripts

--- scripts/install/install_gtest.sh
+++ scripts/install/install_gtest.sh
@@ -6,10 +6,9 @@ sudo apt-get wget
 
 # Build and install
 wget https://github.com/google/googletest/archive/release-1.10.0.tar.gz
-tar -xvzf googletest-release-1.10.0.tar.gz
-cd googletest-release-1.10.0
-mkdir build
-cd build && cmake .. && make -j$(nproc) install
+tar -zxvf release-1.10.0.tar.gz
+cd googletest-release-1.10.0  && mkdir build && \
+cd build && cmake .. && sudo make -j$(nproc) install
 
 # Build and install
 #sudo apt-get install -y libgtest-dev
