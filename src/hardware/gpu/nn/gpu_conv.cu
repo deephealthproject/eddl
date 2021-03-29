f25bfc67b375fb3ef53d34a5686df198291aec7d
salvacarrion <salva.carrion@outlook.com>
2021-03-29 12:54:10 +0200

    Add tensor operation for transposed convolution (GPU)

--- src/hardware/gpu/nn/gpu_conv.cu
+++ src/hardware/gpu/nn/gpu_conv.cu
@@ -393,3 +393,184 @@ void gpu_conv3D_back(ConvolDescriptor3D *D){
 
 
 }
+
+
+
+
+void gpu_conv2DT(ConvolDescriptorT *D) {
+
+    int device=D->I->gpu_device;
+    cudaSetDevice(device);
+
+#ifndef cCUDNN
+    int osize=D->z*D->r*D->c;
+  int isize=D->kz*D->kr*D->kc*D->r*D->c;
+  D->gpuK->ptr=D->K->ptr;
+  D->gpuO->ptr=D->O->ptr;
+  D->gpuI->ptr=D->gpuIB->ptr;
+
+
+  if (D->mem_level>1) {
+    for(int b=0;b<D->I->shape[0];b++,D->gpuO->ptr+=osize) {
+      gpu_im2col_low(D,0,b);
+      gpu_mult2D(D->gpuK,0,D->gpuI,1,D->gpuO,0);
+    }
+  }
+  else {
+
+    gpu_im2col(D,0);
+    if (D->mem_level==0) {
+      gpu_mult2D(D->gpuK,0,D->gpuIB,1,D->gpuOB,0);
+      setDims(D->O);
+      gpu_traspose_batch_depth<<<dimGrid,dimBlock>>>(D->gpuOB->ptr, D->O->ptr, D->O->shape[0], D->z, D->r, D->c);
+      check_cuda(cudaDeviceSynchronize(),"gpu_batch_depth");
+    }
+    else {
+      gpu_im2col(D,0);
+      for(int b=0;b<D->I->shape[0];b++,D->gpuO->ptr+=osize,D->gpuI->ptr+=isize)
+        gpu_mult2D(D->gpuK,0,D->gpuI,1,D->gpuO,0);
+    }
+
+  }
+#else
+    // FWD environment
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    if (D->cudnn_env_init < 0){
+        D->cudnn_env_init = 1;
+        cuDNN_environment_initialization<ConvolDescriptorT>(D);
+    }
+    check_cudnn(cudnnConvolutionForward( hdnn[device], &alpha, D->xDesc, D->I->ptr,
+                                         D->wDesc, D->K->ptr,
+                                         D->convolution_descriptor, D->fwd_algorithm,
+                                         shared_workspace[device], workspace_size[device],
+                                         &beta, D->yDesc, D->O->ptr),"cudnnConvolutionForward",__FILE__);
+#endif
+    if (D->use_bias) {
+#ifndef cCUDNN
+        int size=D->bias->shape[0];
+    for(int i=0;i<size;i+=1024) {
+      int s=min(1024,size-i);
+      gpu_addbias_k<<<D->O->shape[0],s>>>(D->O->ptr, D->O->shape[0], D->r,D->c,D->nk,D->bias->ptr,i);
+      check_cuda(cudaDeviceSynchronize(),"gpu_addbias");
+    }
+#else
+        check_cudnn(cudnnAddTensor(hdnn[device], &alpha, D->bDesc, D->bias->ptr,
+                                   &alpha, D->yDesc, D->O->ptr),"cudnnAddTensor",__FILE__);
+#endif
+    }
+
+
+}
+
+
+void gpu_conv2DT_grad(ConvolDescriptorT *D){
+
+    int device=D->I->gpu_device;
+
+    cudaSetDevice(device);
+    float alpha=1.0;
+    float beta = 0.0;
+#ifndef cCUDNN
+    int osize=D->z*D->r*D->c;
+  int isize=D->kz*D->kr*D->kc*D->r*D->c;
+
+  D->gpugK->ptr=D->gK->ptr;
+  D->gpuD->ptr=D->D->ptr;
+  D->gpuI->ptr=D->gpuIB->ptr;
+
+  if (D->mem_level>1) {
+    for(int b=0;b<D->I->shape[0];b++,D->gpuD->ptr+=osize){
+      gpu_im2col_low(D,0,b);
+      gpu_mult2D(D->gpuD,0,D->gpuI,0,D->gpugK,1);
+    }
+  }
+  else {
+    if (D->mem_level==0) {
+      setDims(D->D);
+      gpu_traspose_batch_depth<<<dimGrid,dimBlock>>>(D->D->ptr, D->gpuOB->ptr, D->z, D->O->shape[0], D->r, D->c);
+      check_cuda(cudaDeviceSynchronize(),"gpu_batch_depth");
+
+      gpu_mult2D(D->gpuOB,0,D->gpuIB,0,D->gpugK,1);
+    }
+    else {
+      for(int b=0;b<D->I->shape[0];b++,D->gpuD->ptr+=osize,D->gpuI->ptr+=isize)
+        gpu_mult2D(D->gpuD,0,D->gpuI,0,D->gpugK,1);
+    }
+  }
+#else
+    check_cudnn(cudnnConvolutionBackwardFilter(hdnn[device], &alpha,
+                                               D->xDesc, D->I->ptr,
+                                               D->yDesc, D->D->ptr, D->convolution_descriptor,
+                                               D->bwd_filter_algorithm,
+                                               shared_workspace[device], workspace_size[device],
+                                               &beta, D->wDesc, D->gK->ptr),"cudnnConvolutionBackwardFilter",__FILE__);
+
+#endif
+    if (D->use_bias) {
+#ifndef cCUDNN
+        int size=D->bias->shape[0];
+    for(int i=0;i<size;i+=1024) {
+      int s=min(1024,size-i);
+      gpu_deltabias_k<<<D->D->shape[0],s>>>(D->D->ptr, D->D->shape[0], D->r,D->c,D->nk,D->gbias->ptr,i);
+      check_cuda(cudaDeviceSynchronize(),"gpu_deltabias");
+    }
+#else
+        check_cudnn(cudnnConvolutionBackwardBias(hdnn[device], &alpha, D->yDesc, D->D->ptr,
+                                                 &beta, D->bDesc, D->gbias->ptr),"cudnnConvolutionBackwardBias",__FILE__);
+#endif
+
+    }
+
+
+}
+
+
+void gpu_conv2DT_back(ConvolDescriptorT *D){
+
+
+    int device=D->I->gpu_device;
+    cudaSetDevice(device);
+#ifndef cCUDNN
+    int osize=D->z*D->r*D->c;
+  int isize=D->kz*D->kr*D->kc*D->r*D->c;
+  D->gpuK->ptr=D->K->ptr;
+  D->gpuD->ptr=D->D->ptr;
+  D->gpuI->ptr=D->gpuIB->ptr;
+
+
+  if (D->mem_level>1) {
+    for(int b=0;b<D->I->shape[0];b++,D->gpuD->ptr+=osize) {
+        gpu_mult2D(D->gpuD, 1, D->gpuK, 0, D->gpuI, 0);
+        gpu_im2col_low(D,1,b);
+    }
+  }
+  else {
+    if (D->mem_level==0) {
+      setDims(D->D);
+      gpu_traspose_batch_depth<<<dimGrid,dimBlock>>>(D->D->ptr, D->gpuOB->ptr,  D->z, D->O->shape[0],D->r, D->c);
+      check_cuda(cudaDeviceSynchronize(),"gpu_batch_depth");
+
+      gpu_mult2D(D->gpuOB, 1, D->gpuK, 0, D->gpuIB, 0);
+      D->gpuI->ptr=D->gpuIB->ptr;
+      gpu_im2col(D,1);
+    }
+    else{
+      for(int b=0;b<D->I->shape[0];b++,D->gpuD->ptr+=osize,D->gpuI->ptr+=isize) {
+          gpu_mult2D(D->gpuD, 1, D->gpuK, 0, D->gpuI, 0);
+      }
+      D->gpuI->ptr=D->gpuIB->ptr;
+      gpu_im2col(D,1);
+    }
+  }
+#else
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    check_cudnn(cudnnConvolutionBackwardData(hdnn[device], &alpha, D->wDesc, D->K->ptr,
+                                             D->yDesc, D->D->ptr,
+                                             D->convolution_descriptor, D->bwd_data_algorithm,
+                                             shared_workspace[device], workspace_size[device],
+                                             &beta, D->xDesc, D->ID->ptr),"cudnnConvolutionBackwardData",__FILE__);
+#endif
+
+}
