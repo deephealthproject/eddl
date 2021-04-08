81974e193412efd61051ea003ab79330a3327ae9
salvacarrion <salva.carrion@outlook.com>
2021-04-08 13:30:07 +0200

    Add pad_back function to tensor (CPU/GPU)

--- tests/tensor/test_tensor_da.cpp
+++ tests/tensor/test_tensor_da.cpp
@@ -51,3 +51,40 @@ TEST(TensorTestSuite, tensor_da_pad){
 #endif
 
 }
+
+TEST(TensorTestSuite, tensor_da_pad_back){
+    int pad;
+
+    // Test #1
+    pad = 1;
+    Tensor* t1 = Tensor::ones({1, 1, 3, 3});
+    Tensor* t1_ref = Tensor::full({1, 1, 3, 3}, 5.0f);
+    Tensor* delta = Tensor::full({1, 1, 3+pad*2, 3+pad*2}, 4.0f);  // 1+4 = 5
+
+    Tensor::pad_back(t1, delta, {pad, pad});
+    ASSERT_TRUE(Tensor::equivalent(t1, t1_ref, 1e-3f, 0.0f, true, true));
+
+    delete t1;
+    delete t1_ref;
+    delete delta;
+
+    // Test GPU
+#ifdef cGPU
+    pad = 5;
+    Tensor* t1_cpu = Tensor::ones({1, 1, 10, 10});
+    Tensor* delta_cpu = Tensor::full({1, 1, 10+pad*2, 10+pad*2}, 4.0f);
+    Tensor* t1_gpu = t1_cpu->clone(); t1_gpu->toGPU();
+    Tensor* delta_gpu = delta_cpu->clone(); delta_gpu->toGPU();
+
+    t1_cpu->pad_back(t1_cpu, delta_cpu, {pad, pad});
+    t1_gpu->pad_back(t1_gpu, delta_gpu, {pad, pad}); t1_gpu->toCPU();
+    ASSERT_TRUE(Tensor::equivalent(t1_cpu, t1_gpu, 1e-3f, 0.0f, true, true));
+
+    delete t1_cpu;
+    delete delta_cpu;
+    delete t1_gpu;
+    delete delta_gpu;
+#endif
+
+
+}
