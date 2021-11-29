15a98fe16d00be42d903108901597d5a6396e774
salvacarrion <salva.carrion@outlook.com>
2021-11-29 15:51:25 +0100

    Improve onnx predict example

--- examples/onnx/7_onnx_import_net_and_predict.cpp
+++ examples/onnx/7_onnx_import_net_and_predict.cpp
@@ -17,26 +17,71 @@
 
 using namespace eddl;
 
+Tensor* preprocess_input_resnet34(Tensor* input, const vector<int> &target_size){
+    // Preprocess image (depends on the model) ***************************
+    // Values from: https://github.com/onnx/models/tree/master/vision/classification/resnet
+
+    auto* mean_vec = new Tensor( {0.485, 0.456, 0.406}, {3, 1}, input->device);
+    auto* std_vec = new Tensor( {0.229, 0.224, 0.225}, {3, 1}, input->device);
+
+    // Check dimension. Input must be a 3D or 4D tensor
+    if(!(input->ndim == 3 || input->ndim == 4)){
+        throw std::runtime_error("A 3D or 4D tensor is expected. " + std::to_string(input->ndim) + "D tensor received.");
+    }
+
+    // Convert from 3D to 4D (if needed)
+    if(input->ndim == 3){
+        input->unsqueeze_(0);
+    }
+
+    // Resize tensor (creates a new instance)
+    Tensor* new_input = input->scale(target_size);  // (height, width)
+
+    // Scale to range [0..1]
+    new_input->mult_(1/255.0f);
+
+    // Normalize: (X-mean)/std
+    // 1) [There is no broadcasting...] Repeat dimensions
+//    Tensor* mean = Tensor::broadcast(mean_vec, new_input);
+//    Tensor* std = Tensor::broadcast(std_vec, new_input);
+    Tensor* mean = Tensor::repeat(mean_vec, target_size[0]*target_size[1], 1);  mean->reshape_(new_input->shape);
+    Tensor* std =  Tensor::repeat(std_vec, target_size[0]*target_size[1], 1); std->reshape_(new_input->shape);
+    new_input->sub_(mean);
+    new_input->div_(std);
+    // ******************************************************************
+
+    delete mean_vec;
+    delete std_vec;
+    delete mean;
+    delete std;
+
+    return new_input;
+}
 
 int main(int argc, char **argv) {
-    // Set default vars
-    string image_fname = "../../examples/data/elephant.jpg";  // Some image
-    string output = "./";
-
-    // Get model and set its input shape
+    // [Manual]
     // Model from: https://github.com/onnx/models/tree/master/vision/classification/resnet
+    // ResNet34 onnx: https://github.com/onnx/models/blob/master/vision/classification/resnet/model/resnet34-v1-7.onnx
+
+    // Set default vars
+    string image_fname = "../../examples/data/elephant.jpg";
+    string class_names_file = "../../examples/data/imagenet_class_names.txt";
     string model_path = "/home/salva/Downloads/models/resnet34-v1-7.onnx";
+
+    // Specific
     int in_channels = 3;
     int in_height = 224;
     int in_width = 224;
 
+    // Import ONNX model
     std::cout << "Importing ONNX..." << std::endl;
     Net *net = import_net_from_onnx_file(model_path, {in_channels, in_height, in_width}, DEV_CPU);  // Why is the device needed?
 
-    // Optional: This model does not include a softmax layer, so we need to add it *****************
+    // **********************************************************************************************
+    // Optional: This model does not include a softmax layer, so we need to add it
     // Get input/output + Add softmax
-    layer in = getLayer(net,"data");  // net->lin[0]
-    layer l = getLayer(net,"resnetv16_dense0_fwd");  // net->lout[0]
+    layer in = net->lin[0];   // getLayer(net,"input_layer_name");
+    layer l = net->lout[0];   // getLayer(net,"output_layer_name");
     layer out = Softmax(l);
 
     // Create model
@@ -59,27 +104,20 @@ int main(int argc, char **argv) {
     // Load test image
     Tensor *image = Tensor::load(image_fname);
 
-    // Preprocess image as the model wants it ***************************
-    // Source: https://github.com/onnx/models/tree/master/vision/classification/resnet
-
-    // Add batch dimension and resize image
-    image->unsqueeze_(0);
-    Tensor *new_image = image->scale({in_height, in_width});
-
-    // Scale to range [0..1]
-    new_image->mult_(1/255.0f);
-    // ******************************************************************
+    // Preprocess input. (Depends on the model)
+    Tensor* image_preprocessed = preprocess_input_resnet34(image, {in_height, in_width});
 
     // Predict image
-    vector<Tensor*> outputs = net->predict({new_image});
+    vector<Tensor*> outputs = net->predict({image_preprocessed});
 
-    //Show probabilities
-    std::cout << "Class probabilities:" << std::endl;
-    outputs[0]->print(2);
-    int best_class = outputs[0]->argmax();
-    float best_class_prob = outputs[0]->ptr[best_class];
-    std::cout << std::setprecision(2) << std::fixed;
-    std::cout << "Selected class: " << best_class << " (" << (best_class_prob * 100.0f) << "%)" << std::endl;
+    // Read class names from txt file
+    std::cout << "Reading class names..." << std::endl;
+    vector<string> class_names = eddl::read_txt_file(class_names_file);
+
+    // Show top K predictions
+    int top_k = 5;
+    std::cout << "Top " << top_k << " predictions:" << std::endl;
+    std::cout << eddl::get_topk_predictions(outputs[0], class_names, top_k)  << std::endl;
 
 	return 0;
 }
