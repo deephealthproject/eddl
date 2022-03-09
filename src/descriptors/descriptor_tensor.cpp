945442c82a86cd10868656ef7e119fdfd36e0440
salvacarrion <salva.carrion@outlook.com>
2022-03-09 14:10:06 +0100

    Update EDDL version

--- src/descriptors/descriptor_tensor.cpp
+++ src/descriptors/descriptor_tensor.cpp
@@ -1,9 +1,9 @@
 
 /*
 * EDDL Library - European Distributed Deep Learning Library.
-* Version: 1.0
-* copyright (c) 2021, Universitat Politècnica de València (UPV), PRHLT Research Centre
-* Date: November 2021
+* Version: 1.1
+* copyright (c) 2022, Universitat Politècnica de València (UPV), PRHLT Research Centre
+* Date: March 2022
 * Author: PRHLT Research Centre, UPV, (rparedes@prhlt.upv.es), (jon@prhlt.upv.es)
 * All rights reserved
 */
